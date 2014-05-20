43650c0e57eca4dd8bf9e622c6be3ac9557569d0
Eric Klavins <klavins@uw.edu>
2014-05-19 18:37:27 -0700

    I/O info in Oyster file and Pouring a gel

--- fragments/pour_gel.pl
+++ fragments/pour_gel.pl
@@ -1,4 +1,28 @@
 require "aqualib:lib/util.pl"
 require "aqualib:lib/cloning.pl"
 
-print("Pour Gel","TODO")
+argument
+  FO: generic
+end
+
+fragments_per_gel = 10
+num_fragments = length(FO[:fragments])
+num_gels = floor(num_fragments / fragments_per_gel)
+
+print("Number of gels to pour",num_gels)
+
+i=0
+gels = []
+
+while i<num_gels
+  produce silently
+    gel = 1 "Gel"
+    data
+      matrix: array_same(-1,12)
+    end  
+  end
+  gels = append(gels,gel)
+  i = i+1
+end
+
+print("Gel IDs",gels)
