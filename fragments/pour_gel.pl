fb7b7db710e51f18c0530618691247653292907f
Eric Klavins <klavins@uw.edu>
2014-05-23 13:54:00 -0700

    gel labels

--- fragments/pour_gel.pl
+++ fragments/pour_gel.pl
@@ -9,7 +9,7 @@ fragments_per_gel = 10.0
 num_fragments = length(FO[:fragments])
 num_gels = ceil(num_fragments / fragments_per_gel)
 
-print("Pour %{num_gels} gel(s)","")
+
 
 i=0
 gels = []
@@ -20,6 +20,8 @@ while i<num_gels
 end
 
 FO[:gels] = ha_select(gels,:id)
+
+print("Pour %{num_gels} gel(s)","Label it(them) with the item number(s) " + gels)
 
 log
   return: { FO: FO }
