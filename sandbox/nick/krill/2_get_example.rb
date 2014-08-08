59b63a9cbb18cc1bc63fec2796922120b3ad90b9
Nick Bolten <nbolten@gmail.com>
2014-08-08 13:32:56 -0700

    Added get example

---
+++ sandbox/nick/krill/2_get_example.rb
@@ -0,0 +1,17 @@
+class Protocol
+  def main
+    data = show {
+      title "An input example"
+      get "text", var: "y", label: "Enter a string", default: "Hello World 1"
+      get("text", var="z", label="Enter a string", default="Hello World 2")
+    }
+
+    y = data[:x]
+    z = data[:z]
+
+    show {
+      note y
+      note z
+    }
+  end
+end
