e2144d91d66e8df5eed91d6da57f89fe172485dd
christakahashi <cnt@u.washington.edu>
2014-08-08 14:57:21 -0700

    Update test.rb

--- sandbox/chris/test.rb
+++ sandbox/chris/test.rb
@@ -1,9 +1,11 @@
 class Protocol
   def main
     foo = find(:item, sample: { object_type: { name: "Enzyme Stock" }, sample: { name: "Phusion HF Master Mix" } } )
+    l = foo.length
+    xx = 3
     show {
       title "Hello World!"
-      note "#{foo}"
+      note "#{l} xx: #{xx}"
     }
   end
 end
