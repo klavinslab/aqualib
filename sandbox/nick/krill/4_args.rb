ac44215ecfa14e961776b4a3047debb8564aa88d
Nick Bolten <nbolten@gmail.com>
2014-08-08 13:48:56 -0700

    added args test

---
+++ sandbox/nick/krill/4_args.rb
@@ -0,0 +1,16 @@
+class Protocol
+  def arguments
+    { a: 1,
+      b: "banana"
+    }
+  end
+
+  def main
+    # Can I unpack multiple?
+    a, b = input[:a], input[:b]
+    show {
+      note: a
+      note: b
+    }
+  end
+end
