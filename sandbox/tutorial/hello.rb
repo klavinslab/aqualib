f1259fba80ca386805540264323fb8f4a6a46a4c
Eric Klavins <klavins@uw.edu>
2014-08-08 13:25:49 -0700

    Update hello.rb

--- sandbox/tutorial/hello.rb
+++ sandbox/tutorial/hello.rb
@@ -1,7 +1,13 @@
 class Protocol
   def main
     show {
-      title "Hello World!"
+      title "Hello World!" 
+      (1..10).each { |i|
+        note "i = #{i}"
+      }
+    }
+    show {
+      title "Thanks :-)"
     }
   end
 end
