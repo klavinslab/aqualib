42be1489e77fba008ac507004a5a4e0817ec38f4
lmbrettner <leandra.brettner@gmail.com>
2014-08-08 14:53:45 -0700

    Update test.rb

--- sandbox/Leandra/test.rb
+++ sandbox/Leandra/test.rb
@@ -2,9 +2,13 @@ class Protocol
   def main
     data = show {
       title "Hello World!" 
+      (1..10).each { |i|
+        get "number",  var: "g#{i}"
+      }
     }
     show {
       title "Thanks :-)"
+      note data["g3".to_sym]
     }
   end
 end
