d7bf6512a937bad4e271a2c602cf61dd5dc4d9c7
Eric Klavins <klavins@uw.edu>
2014-08-20 10:49:11 -0700

    daily/weekly tasks

--- periodic/tasks.rb
+++ periodic/tasks.rb
@@ -45,6 +45,46 @@ class Protocol
           select [ "Yes", "No" ], var: "done", label: "Did you complete the task?", default: 1
         }
 
+        if data[:done] == "Yes"
+
+          task.status = "done"
+          task.save
+          show {
+            title "Thank you!"
+            note "Aquarium has made a note of your efforts"
+          }
+
+        else
+
+          task.status = "ready"
+          task.save
+
+        end
+
+        if data[:done] == "No" || tasks.length > 1
+
+          show {
+            title "Another task?"
+            select ["Yes","No"], var: "choice", label: "Do you have time for another #{type} task?", default: 1
+          }
+
+          if more == "Yes"
+            tasks = find(:task,{task_prototype: {name: type},status: "ready"})
+          else
+            tasks = []
+          end
+
+        else
+
+          show {
+            title "Done with daily tasks."
+            note "There are no more #{type} tasks."
+          }
+
+          tasks = []
+
+        end
+
       end
 
     end
@@ -54,94 +94,5 @@ class Protocol
 end
 
 
-
-# if length(tasks) == 0
-
-#   step
-#     description: "All %{type} tasks have been completed or are in progress."
-#     note: "Thanks for checking!"
-#   end
-
-# else
-
-#   while length(tasks) > 0
-
-#     step
-#       description: "Available %{type} Tasks"
-#       note: "The following tasks have not been completed today."
-#       getdata
-# 	name: string, "Choose a task", ha_select(tasks,:name)
-#       end
-#     end
-
-#     task = ha_get(tasks,:name,name)
-
-#     set_task_status(task,"working")
-
-#     step
-#       description: task[:name]
-#       foreach n in task[:specification][:notes]
-#         note: n
-#       end
-#       foreach c in task[:specification][:checks]
-#         check: c
-#       end 
-#       foreach w in task[:specification][:warnings]
-#         warning: w
-#       end
-#       foreach i in task[:specification][:images]
-#         image: i
-#       end
-#       getdata
-#         done: string, "Did you complete the task?", ["Yes", "No"]
-#       end
-#     end
-
-#     if done == "Yes"
-
-#       set_task_status(task,"done")
-
-#       step
-#         description: "Thank you!"
-#         note: "Aquarium has made a note of your efforts."
-#       end
-
-#       log
-#         TASK: { task: task }
-#       end
-
-#     else
-
-#       set_task_status(task,"ready")
-
-#     end
-
-#     if done == "No" || length(tasks) > 1
-
-#       step
-#         description: "Another task?"
-#         getdata
-#           more: string, "Do you have time to do another %{type} task?", [ "Yes", "No" ]
-#         end
-#       end
-     
-#       if more == "Yes"
-#         tasks = tasks(type,"ready")
-#       else
-#         tasks = []
-#       end
-
-#     else
-
-#       step
-#         description: "Done with daily tasks."
-#         note: "There are no more %{type} tasks."
-#       end
-
-#       tasks = []
-
-#     end
-
-#   end
 
 # end
