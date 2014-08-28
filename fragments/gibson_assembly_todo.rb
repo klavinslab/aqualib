572f3779fabeeab7568056d3f0c28b67d4adc504
Eric Klavins <klavins@uw.edu>
2014-08-28 11:32:27 -0700

    working with gibson assembly tasks

--- fragments/gibson_assembly_todo.rb
+++ fragments/gibson_assembly_todo.rb
@@ -6,17 +6,24 @@ class Protocol
 		# This protocol determines the set of all fragments that need to be made
 		# for the current list of Gibson Assemblies. 
 
-		tasks = find(:task,{task_prototype: { name: "Gibson Assembly" }}).collect { |t| 
+		tasks = find(:task,{task_prototype: { name: "Gibson Assembly" }})
+
+		show {
+			title "Tasks"
+			note: tasks
+		}
+
+		task_details = tasks.collect { |t| 
         	{
         		id: t.id,
         		fragments: t.spec[:fragments].collect { |fid| find(:sample, { id: fid } ) }
         	}
         }
 
-		show {
-			title "Fragments Required for Current Gibson Assmebly Tasks"
-			table tasks.collect { |t| [ t[:id], t[:fragments].collect { |f| f.id } ] }
-		}
+		#show {
+		#	title "Fragments Required for Current Gibson Assmebly Tasks"
+		#	table tasks.collect { |t| [ t[:id], t[:fragments] }
+		#}
 
 	end
 
