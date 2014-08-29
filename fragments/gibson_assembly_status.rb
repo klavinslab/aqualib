68b91b0e2b00ed9805f87a5a72222c184a6b764f
Eric Klavins <klavins@uw.edu>
2014-08-29 10:43:44 -0700

    working with gibson assembly tasks

---
+++ fragments/gibson_assembly_status.rb
@@ -0,0 +1,20 @@
+include "aqualib/lib/cloning"
+
+class Protocol
+
+	include Cloning
+
+	def main
+
+	  gas = gibson_assembly_status
+
+	  show {
+	  	title "Status"
+	  	note gas.to_s
+	  }
+
+	  return gas
+
+	end
+
+end
