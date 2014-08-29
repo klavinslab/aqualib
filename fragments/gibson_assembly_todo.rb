d5c5f53aad96dbb259866ba65295caefc56e9fb1
Eric Klavins <klavins@uw.edu>
2014-08-29 09:08:53 -0700

    working with gibson assembly tasks

--- fragments/gibson_assembly_todo.rb
+++ fragments/gibson_assembly_todo.rb
@@ -7,11 +7,18 @@ class Protocol
 		# for the current list of Gibson Assemblies. 
 
 		tasks = find(:task,{task_prototype: { name: "Gibson Assembly" }})
+		fragments = (tasks.collect { |t| find(:sample,{id:t.spec[:fragments]}) }).flatten
 
 		show {
 			title "Tasks"
-			note tasks
+			table tasks.collect { |t| [ t.id, t.name ] }
 		}
+
+		show {
+			title "Fragments"
+			table fragments.collect { |f| [ f.id, f.name ] }
+		}
+
 
 		task_details = tasks.collect { |t| 
         	{
