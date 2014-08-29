191199cb3bec5c55cd30613f02765d6536edbc32
Eric Klavins <klavins@uw.edu>
2014-08-29 10:50:49 -0700

    working with gibson assembly tasks

--- fragments/gibson_assembly_status.rb
+++ fragments/gibson_assembly_status.rb
@@ -9,8 +9,15 @@ class Protocol
 	  gas = gibson_assembly_status
 
 	  show {
-	  	title "Status"
-	  	note gas.to_s
+	  	title "Gibson Assemblies in the Pipeline"
+	  	note "Under Construction: #{gas[:assemblies][:under_construction]}"
+		note "Waiting for Ingredients: #{gas[:assemblies][:waiting_for_ingredients]}"
+	  	note "Ready to Build: #{gas[:assemblies][:ready_to_build]}"		
+	  	note "Out for Sequencing: #{gas[:assemblies][:out_for_sequencing]}"		
+	  	separator
+	  	note "Fragments Ready To Use: #{gas[:fragments][:ready_to_use]}"
+	  	note "Fragments Ready To Build: #{gas[:fragments][:ready_to_build]}"
+	  	note "Fragments Not Ready To Build: #{gas[:fragments][:not_ready_to_build]}"	  	
 	  }
 
 	  return gas
