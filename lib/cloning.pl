a3cf3b1f0ac51a3e06b698adccf5758175bc543e
Eric Klavins <klavins@uw.edu>
2014-09-02 14:13:23 -0700

    filtered out deleted items in choose_object

--- lib/cloning.pl
+++ lib/cloning.pl
@@ -1,108 +1,53 @@
-module Cloning
+function fragment_info(fid)
 
-  def fragment_info fid
+  # Gets information about a fragment, including its name, template info, and primer info
 
-    fragment = find(:sample,{id: fid})[0]# Sample.find(fid)
-    props = fragment.properties
+  local frag = find(:sample,{id: fid})
 
-    fwd = props["Forward Primer"]
-    rev = props["Reverse Primer"]
-    template = props["Template"]
+  if frag == [] || frag[0][:sample_type][:name] != "Fragment"
+    return { error: "Could not find fragment with id ${fid}" }
+  end
 
-    if fwd == nil || rev == nil || template == nil
+  local temp = find(:item,{ sample: { name: frag[0][:field3]},  object_type: { name: "Plasmid Stock" }  } ).reject { |i| i.location == 'deleted '}
+  local fwds = find(:item,{ sample: { name: frag[0][:field4] }, object_type: { name: "Primer Aliquot" } } ).reject { |i| i.location == 'deleted '}
+  local revs = find(:item,{ sample: { name: frag[0][:field5] }, object_type: { name: "Primer Aliquot" } } ).reject { |i| i.location == 'deleted '}
 
-      return nil # Whoever entered this fragment didn't provide infor on how to make it
+  if length(temp) == 0 
+    return { error: "Template stock(s) " + frag[0][:field3] +  " missing for fragment %{fid}." }
+  end
 
-    else
+  if length(fwds) == 0 
+    return { error: "Forward primer stock(s) " + frag[0][:field4] + " missing for fragment %{fid}."  }
+  end
 
-      fwd_items = fwd.in "Primer Aliquot"
-      rev_items = rev.in "Primer Aliquot"
-      template_items = template.in "Plasmid Stock"
-
-      if fwd_items.length == 0 || rev_items.length == 0 || template_items.length == 0
-
-        return nil # There are missing items
-
-      else
-
-        t1 = fwd_items[0].sample.properties["Tm Anneal"] || 70.0
-        t2 = rev_items[0].sample.properties["Tm Anneal"] || 70.0
-
-        stocks = fragment.items.select { |i| i.object_type.name == "Fragment Stock" }
-
-        return {
-          fragment: fragment,
-          stocks: stocks,
-          fwd: fwd_items[0],
-          rev: rev_items[0],
-          template: template_items[0],
-          tanneal: (t1+t2)/2.0
-        }
-
-      end
-
-    end
-
-  end # # # # # # # 
-
-  def gibson_assembly_status
-
-    # find all un done gibson assembly tasks 
-  tasks = find(:task,{task_prototype: { name: "Gibson Assembly" }})
-
-  ready = tasks.select { |t| 
-    t.status == "ready" 
-  }
-
-  running = tasks.select { |t| 
-    t.status == "running"
-  }
-
-  out = tasks.select { |t| 
-    t.status == "out for sequencing" 
-  }
-
-  # look up all fragments needed to 
-  ready.each do |t|
-
-    t[:fragments] = {
-      ready_to_use: [],
-      ready_to_build: [],
-      not_ready_to_build: []
-    }
-
-    t.simple_spec[:fragments].each do |fid|
-
-      info = fragment_info fid
-
-      if !info
-        t[:fragments][:not_ready_to_build].push fid
-      elsif info[:stocks].length > 0
-        t[:fragments][:ready_to_use].push fid
-      else
-        t[:fragments][:ready_to_build].push fid
-      end
-
-    end
-
+  if length(revs) == 0 
+    return { error: "Reverse primer stock(s) " + frag[0][:field4] + " missing for fragment %{fid}." }
   end
 
   return {
-
-    fragments: ((tasks.select { |t| t.status == "ready" }).collect { |t| t[:fragments] })
-      .inject { |all,part| all.each { |k,v| all[k].concat part[k] } },
-
-    assemblies: {
-      under_construction: running.collect { |t| t.id },
-      waiting_for_ingredients: (ready.select { |t| t[:fragments][:ready_to_build] != [] || t[:fragments][:not_ready_to_build] != [] }).collect { |t| t.id },
-      ready_to_build: (ready.select { |t| t[:fragments][:ready_to_build] == [] && t[:fragments][:not_ready_to_build] == [] }).collect { |t| t.id },
-        out_for_sequencing: out.collect { |t| t.id }
-      }
-
+    fragment_id: fid,
+    fragment_name: frag[0][:name],
+    template_id:   temp[0][:id],
+    template_name: temp[0][:sample][:name],
+    forward_primer_id:   fwds[0][:id],
+    forward_primer_name: fwds[0][:sample][:name],
+    reverse_primer_id:   revs[0][:id],
+    reverse_primer_name: revs[0][:sample][:name]
   }
-
-  end # # # # # # # 
 
 end
 
+function new_gel()
 
+  # makes a new 12 lane gel
+
+  produce silently
+    gel = 1 "Gel"
+    data
+      matrix: [array_same(-1,12)]
+    end  
+  end
+
+  return gel
+
+end
