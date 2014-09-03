519cf160bb07cd0a924ef115556556eeeabb88df
Eric Klavins <klavins@uw.edu>
2014-09-03 12:14:07 -0700

    working on delete

--- utils/delete_old_stuff.rb
+++ utils/delete_old_stuff.rb
@@ -3,18 +3,13 @@ class Protocol
 	def main
 
 		projects = find :project, {}
-
-		project = (show {
-			title "Choose a project"
-			select projects, var: "project", label: "Select Project Name"
-		})[:project]
-
 		sample_types = find(:sample_type,{})
 
-		sample_type = (show {
-			title "Choose a sample type"
+		data = show {
+			title "Choose item types to delete"
+			select projects, var: "project", label: "Select Project Name"
 			select sample_types, var: "sample_type", label: "Select Sample Type"
-		})[:sample_type]
+        }
 
 
 	end
