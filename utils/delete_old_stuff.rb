513fb59726f55633a70c2ef82c566a515cc1b765
Eric Klavins <klavins@uw.edu>
2014-09-03 16:34:16 -0700

    reindent

--- utils/delete_old_stuff.rb
+++ utils/delete_old_stuff.rb
@@ -1,68 +1,68 @@
 class Protocol
 
-	def main
+  def main
 
-		another = true
+    another = true
 
-		while another
+    while another
 
-			projects = find :project, {}
-			sample_types = find(:sample_type,{})
+      projects = find :project, {}
+      sample_types = find(:sample_type,{})
 
-			data = show {
-				title "Choose project and sample type"
-				select projects, var: "project", label: "Select Project Name"
-				select sample_types.collect { |st| st.name }, var: "sample_type", label: "Select Sample Type"
-	        }
+      data = show {
+        title "Choose project and sample type"
+        select projects, var: "project", label: "Select Project Name"
+        select sample_types.collect { |st| st.name }, var: "sample_type", label: "Select Sample Type"
+      }
 
-	        project 	= data[:project]
-	        sample_type = find(:sample_type,{name: data[:sample_type]})[0]
+      project   = data[:project]
+      sample_type = find(:sample_type,{name: data[:sample_type]})[0]
 
-			data = show {
-				title "Choose container type"
-				select sample_type.object_types.collect { |ot| ot.name }, var: "object_type_name", label: "Select Object Type"
-			}
+      data = show {
+        title "Choose container type"
+        select sample_type.object_types.collect { |ot| ot.name }, var: "object_type_name", label: "Select Object Type"
+      }
 
-			object_type_name = data[:object_type_name]
+      object_type_name = data[:object_type_name]
 
-			items = find( :item, { sample: { project: project }, object_type: { name: data[:object_type_name] } } )
+      items = find( :item, { sample: { project: project }, object_type: { name: data[:object_type_name] } } )
 
-			if items.length > 0
+      if items.length > 0
 
-				data = show {
-					title "Choose items"
-					select items.collect { |i| "#{i.id}: #{i.sample.name} at #{i.location}" }, var: "item_list", label: "Select Items", multiple: true
-				}
+        data = show {
+          title "Choose items"
+          select items.collect { |i| "#{i.id}: #{i.sample.name} at #{i.location}" }, var: "item_list", label: "Select Items", multiple: true
+        }
 
-				item_ids = data[:item_list].collect { |i| i.split(':')[0].to_i }
+        item_ids = data[:item_list].collect { |i| i.split(':')[0].to_i }
 
-				show {
-					title "Deleted!"
-					note item_ids
-				}
+        show {
+          title "Deleted!"
+          note item_ids
+        }
 
-				item_ids.each { |iid| 
-					i = find(:item,{id: iid})[0]
-					touch i
-					i.mark_as_deleted 
-				}
+        item_ids.each { |iid| 
+          i = find(:item,{id: iid})[0]
+          touch i
+          i.mark_as_deleted 
+        }
 
-			end 
+      end 
 
-			data = show {
-				if items.length == 0
-					title "No items"
-					note "There are no items with container type #{object_type_name} associated with #{project}" 
-				else
-					title "More?"
-				end
-				select [ "Yes", "No" ], var: "more", label: "Delete more items?"
-			}
+      data = show {
+        if items.length == 0
+          title "No items"
+          note "There are no items with container type #{object_type_name} associated with #{project}" 
+        else
+          title "More?"
+        end
+        select [ "Yes", "No" ], var: "more", label: "Delete more items?"
+      }
 
-			another = (data[:more] == "Yes")
+      another = (data[:more] == "Yes")
 
-		end
+    end
 
-	end
+  end
 
 end
