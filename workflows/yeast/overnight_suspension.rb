c446229b5e2c642e6af34b32a74a055bb02fc4bc
sunshineyyy <yaoyu.yang88@gmail.com>
2014-09-18 16:00:37 -0700

    update

--- workflows/yeast/overnight_suspension.rb
+++ workflows/yeast/overnight_suspension.rb
@@ -13,24 +13,24 @@ class Protocol
   def arguments
     {
       #Enter the item id that you are going to start overnight with
-      item_ids: [8437,8431,8426],
+      yeast_item_ids: [8437,8431,8426],
       media_type: "YPAD"
     }
   end
 
   def main
-  	items = []
+  	yeast_items = []
   	overnights = []
   	input[:item_ids].each do |itd|
-  		item = find(:item, id: itd)
-  		items.push item
-  		# name = item.sample.name
-  		# overnight = produce new_sample name, of: "Yeast Strain", as: "Yeast Overnight Suspension"
-  		# overnights.push overnight
+  		yeast_item = find(:item, id: itd)
+  		yeast_items.push yeast_item
+  		name = yeast_item.sample.name
+  		overnight = produce new_sample name, of: "Yeast Strain", as: "Yeast Overnight Suspension"
+  		overnights.push overnight
   	end
 
-  	take items, interactive: true
-  	# release overnights, interactive: true
+  	take yeast_items, interactive: true
+  	release overnights, interactive: true
 
   end
 
