25f4ed19c9aa2bb4983703a08dfe823c07bc7794
Nick Bolten <nbolten@gmail.com>
2014-09-18 11:12:08 -0700

    tweaking steak_plates

---
+++ sandbox/nick/tests/produce_test.rb
@@ -0,0 +1,5 @@
+class Protocol
+  def main
+    x = produce new_object("1 L Bottle")
+  end
+end
---
+++ sandbox/nick/tests/two_titles.rb
@@ -0,0 +1,8 @@
+class Protocol
+  def main
+    show {
+      title "Title 1"
+      title "Title 2"
+    }
+  end
+end
