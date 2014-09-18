8bca9a0cf3ce451d278636e225cffa4e7c253160
Nick Bolten <nbolten@gmail.com>
2014-09-18 13:30:56 -0700

    tweaking steak_plates

--- sandbox/nick/streak_plates.rb
+++ sandbox/nick/streak_plates.rb
@@ -2,11 +2,25 @@ needs "aqualib/lib/standard"
 
 class Protocol
   def arguments
-    {plate_ids: [4977]}
+    {plate_ids: [4977] # Replace with e.g. "plate_ids Plasmid => [0]"
+    }
   end
 
   def main
-    input_plates = input[:plate_ids]
+    plate_ids = input[:plate_ids]
+
+    # Figure out the types / number of media needed for each plate (media = array)
+    plate_ids.each do |pid|
+      plate_sample = find(:item, id: pid)[0].sample
+      show {
+        note plate_sample
+      }
+    end
+    # Take out the right number of each plate type (array -> counter hash)
+    # Streak out strain i onto plate of type media[i]
+    # Produce each plate after streaking, consuming the taken plate
+    # Put away the produced plates
+    # Put away the plates that were taken out
     show {
       note input_plates.to_s
     }
