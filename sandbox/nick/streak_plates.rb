c1c6df40c5b5b468d67496ffd22bb7bf253fe3d9
Nick Bolten <nbolten@gmail.com>
2014-09-18 11:17:10 -0700

    tweaking steak_plates

--- sandbox/nick/streak_plates.rb
+++ sandbox/nick/streak_plates.rb
@@ -2,13 +2,11 @@ needs "aqualib/lib/standard"
 
 class Protocol
   def arguments
-    {plate_ids: find(:item, sample: {object_type: {name: "E coli Plate"}})[0]
-    #Item.where("object_type='E coli Plate'")[0]
-    }
+    {plate_ids: [4977]}
   end
 
   def main
-    bacterial_plates = input["bacterial_plates"]
+    input_plates = input["bacterial_plates"]
     show {
       note bacterial_plates
     }
