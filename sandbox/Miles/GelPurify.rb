154dc53b1d3b59e72e9b8259ea19a81740173e5c
MilesGander <gandermiles@gmail.com>
2014-09-18 10:28:39 -0700

    Update GelPurify.rb

--- sandbox/Miles/GelPurify.rb
+++ sandbox/Miles/GelPurify.rb
@@ -4,23 +4,44 @@ needs "aqualib/lib/standard"
 needs "aqualib/lib/cloning"
 
 class Protocol
+	
 
 
 include Standard
 include Cloning
 
+
 def arguments
 	{
-	gelslice_ids: 
+	gelslice_ids: SampleType.where("name='Gel Slice'")[0] 
 
-  def debug
-    true
-  end
-
-	def main
+def main
+	
+	slices = find(:item, {sample: input[:gelslice_ids]})
+	
+	slice_number = slices.length
 	
 	show{
 		title "This protocol purfies gel slices int DNA fragment stocks."
 	}
-	end
+	
+	take slices, interactive: true,  method: "boxes"
+	
+	show{
+		title "Lable the gel slice tubes with the following numbers."
+		table [["Tube", "Number"],[[slices], [1:slice_number]]]
+	}
+	
+	data = show{
+		title: "Weigh the gel slices."
+		check: "Zero the scale"
+		check: "Weigh each slice and record it's weight on the side of the tube in grams."
+		note: "Enter the recorded weights below."
+		slices.each{
+			get "number", var: "w#{slices.id}"
+		}
+	}	
+	
+	w = slices.collect{ |slice_number| data["w#{slices.id}".to_sym]}
+	
 end
