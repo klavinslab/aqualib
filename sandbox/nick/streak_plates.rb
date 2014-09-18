3d1caee4e87d702c8f4845efe4e32201290e7e55
Nick Bolten <nbolten@gmail.com>
2014-09-18 11:13:17 -0700

    tweaking steak_plates

--- sandbox/nick/streak_plates.rb
+++ sandbox/nick/streak_plates.rb
@@ -2,7 +2,7 @@ needs "aqualib/lib/standard"
 
 class Protocol
   def arguments
-    { plate_ids: Sample.where("sample_type='E coli Plate'")[0]
+    { plate_ids: Item.where("object_type='E coli Plate'")[0]
     }
   end
 
