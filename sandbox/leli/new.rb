8a6aa413d6f5f8ec980840e095143b8fe33d6dfe
tamimeur <tamimeur@gmail.com>
2014-09-18 10:42:00 -0700

    Update new.rb

--- sandbox/leli/new.rb
+++ sandbox/leli/new.rb
@@ -2,10 +2,10 @@
 class Protocol
   def main
     
-    m = [
-      [ "A", "Very", "Nice", { content: "Table", style: { color: "#f00"}}],
-      [{content: 1, check: true},2,3,4]
-    ]
+    #m = [
+    #  [ "A", "Very", "Nice", { content: "Table", style: { color: "#f00"}}],
+    #  [{content: 1, check: true},2,3,4]
+    #]
     
     data = show {
       title "Hello World!"
