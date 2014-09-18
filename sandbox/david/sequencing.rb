a0f864f6e7dd74a4a13ae2184eab8a803831986d
David Younger <dyounger@gmail.com>
2014-09-18 10:28:34 -0700

    Update sequencing.rb

--- sandbox/david/sequencing.rb
+++ sandbox/david/sequencing.rb
@@ -1,7 +1,33 @@
+needs "aqualib/lib/standard"
+needs "aqualib/lib/cloning"
+
 class Protocol
-  def main
-    show {
-      title "Hello World!"
+  
+  include Standard
+  include Cloning
+  
+  def debug
+    false
+  end
+  
+  def arguments
+    {
+      x:1, y: "name"
     }
+  end
+  
+  
+  
+  def main
+   
+   x = input[:x]
+    y = input[:name]
+
+    show 
+      {
+        title "Arguments"
+        note "x = #{x}, y = #{y}"
+      }
+  
   end
 end
