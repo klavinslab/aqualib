4e31e95570bc21aa28365108ab9e557ddd5196db
tamimeur <tamimeur@gmail.com>
2014-09-18 10:06:41 -0700

    Update new.rb

--- sandbox/leli/new.rb
+++ sandbox/leli/new.rb
@@ -3,6 +3,10 @@ class Protocol
   def main
     show {
       title "Hello World!"
+      title "An input example."
+      get "text", var: "y", label:"Enter a string", default: "hi again"
     }
+    
+    y = data[:y]
   end
 end
