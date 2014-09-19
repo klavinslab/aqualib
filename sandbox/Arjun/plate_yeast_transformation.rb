fb6d53d4d99640dd3563fa18735cd95444ebf2c5
arjunkhakhar <arjunkhakhar@gmail.com>
2014-09-18 20:23:28 -0700

    Update plate_yeast_transformation.rb

--- sandbox/Arjun/plate_yeast_transformation.rb
+++ sandbox/Arjun/plate_yeast_transformation.rb
@@ -21,6 +21,19 @@ class Protocol
     
     take yeast_transformation_mixtures, interactive: true
     
+    show{
+      title "Resuspending in water"
+      check "Spin down all the yeast transformation mixtures in samll table top centrifuge for ~1 minute"
+      check "Pipette off supernatant being careful not to disturb yeast pellet"
+    }
+    
+    show{
+      title "Resuspending in water"
+      check "Add 200ul of sterile water to each eppendorf tube"
+      check "Resuspend the pellet by vortexing the tube throughly"
+      warning "Make sure the pellet is resuspended and there are no cells stuck to the bottom of the tube"
+    }
+    
     counter=0
     plates = []
     yeast_transformation_plate_ids=[]
@@ -33,22 +46,37 @@ class Protocol
         plate = choose_object "SDO -Ura Plate (sterile)"
         plates.push([plate])
         
-        show{
-          note "Label plate with your initials, the date, the initials #{input[:initials]} and the ID:
-        
-        
-        
       elseif selections[counter]=="TRP"
-      
+        plate = choose_object "SDO -Trp Plate (sterile)"
+        plates.push([plate])
+        
       elseif selections[counter]=="LEU"
-      
+        plate = choose_object "SDO -Leu Plate (sterile)"
+        plates.push([plate])
+        
       elseif selections[counter]=="HIS"
-      
+        plate = choose_object "SDO -His Plate (sterile)"
+        plates.push([plate])
+        
       else
         #get statement for the selection plate
       end
       
+      show{
+        check "Label plate with your initials, the date, the initials #{input[:initials]} and the ID: #{j[:id]}"
+        check "Flip the plate and add 4-5 glass beads to it"
+        check "Add 200ul of the transformation mixture from the tube labeled #{transformation_mixture[:id]}"
+      }      
+      
       counter=counter+1
+    end
+    
+    show{
+      check "Shake the plates in all directions to evely spread the culture over its surface till its dry"
+    end
+    
+    show{
+      check "Put the plates with the agar side up in the 30C incubator"
     end
     
   end
