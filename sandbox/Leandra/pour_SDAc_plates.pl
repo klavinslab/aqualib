fb88d0bc4224b76bc363b6b1d799c48ce0dca55d
lmbrettner <leandra.brettner@gmail.com>
2014-09-24 16:13:30 -0700

    Update pour_SDAc_plates.pl

--- sandbox/Leandra/pour_SDAc_plates.pl
+++ sandbox/Leandra/pour_SDAc_plates.pl
@@ -58,6 +58,7 @@ end
 
 antibiotic_number = [ amp_number, kan_number ]
 antibiotic_name = [ antibiotic_amp, antibiotic_kan ] # Initialize global variable
+antibiotic_aliquots = antibiotic_number antibiotic_name
 
 take
 antibiotic_aliquots = antibiotic_number antibiotic_name
