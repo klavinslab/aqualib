4c3cc348c5efbff17a4cc4360f51943c8b2fdb96
Eric Klavins <klavins@uw.edu>
2014-09-30 12:54:54 -0700

    timers

---
+++ sandbox/klavins/timers.rb
@@ -0,0 +1,18 @@
+class Protocol
+
+  show {
+    title "Default Timer"
+    timer
+  }
+
+  show {
+    title "Initial Time Specified"
+    timer initial: { hours: 0, minutes: 0, seconds: 20 }
+  }
+
+  show { 
+    title: "Count Up"
+    timer final: { hours: 0, minutes: 0, seconds: 10 }, direction: "up"
+  }
+
+end
