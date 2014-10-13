41dda3372be0bf19011f9ca4caae578d8c3f2b22
sunshineyyy <yaoyu.yang88@gmail.com>
2014-10-12 23:00:11 -0700

    refactor gel_purify

--- workflows/ecoli/030_cut_gel.rb
+++ workflows/ecoli/030_cut_gel.rb
@@ -3,18 +3,15 @@ class Protocol
 
   include Standard
 
-  def debug
-    false
-  end
-
   def arguments
     {
       io_hash: {},
-      gel_ids: [28130]
+      gel_ids: [28420],
+      debug_mode: "Yes"
     }
   end
 
-  def gel_band_verify col, options = {}
+  def gel_band_verify_cut col, options = {}
     m = col.matrix
     routes = []
     opts = { except: [] }.merge options
@@ -27,15 +24,55 @@ class Protocol
     	    routes.push lane: [i,j], length: length
     	  end
     	end
-	end
+	  end
 
-	show {
-		title "Verify that each lane matches expected size"
-		table [[ "Row", "Column", "Expected fragment size" ]].concat( routes.collect { |r| [ r[:lane][0]+1, r[:lane][1]+1, r[:length]] } )
-		}
+  	verify_data = show {
+  		title "Verify that each lane matches expected size"
+  		# table [[ "Row", "Column", "Expected fragment size" ]].concat( routes.collect { |r| [ r[:lane][0]+1, r[:lane][1]+1, { content: r[:length], check: true }] } )
+      routes.each_with_index do |r,idx|
+        select ["Yes", "No"], var: "verify#{r[:lane][0]}_#{r[:lane][1]}", label: "Does gel lane on Row #{r[:lane][0]+1} Col #{r[:lane][1]+1} match the expected length of #{r[:length]} bp"
+      end
+  		}
 
-	return routes
-  end
+    # show {
+    #   routes.each do |r|
+    #     note "#{verify_data[:"verify#{r[:lane][0]}_#{r[:lane][1]}".to_sym]}"
+    #     note "#{m[r[:lane][0]][r[:lane][1]]}"
+    #   end
+    # }
+
+    routes.each do |r| 
+      m[r[:lane][0]][r[:lane][1]] = 3.14 if verify_data[:"verify#{r[:lane][0]}_#{r[:lane][1]}".to_sym] == "No"
+    end
+
+    items = []
+    new_routes = []
+    (0..m.length-1).each do |i|
+      (0..m[i].length-1).each do |j|
+        if m[i][j] > 0 && ! ( opts[:except].include? [i,j] )
+          if m[i][j] == 3.14
+            new_routes.push lane: [i,j], slice_id: "Do not cut", length: "NA"
+          else
+            s = find(:sample,{id: m[i][j]})[0]
+            length = s.properties["Length"]
+            item = s.make_item "Gel Slice"
+            items.push item
+            new_routes.push lane: [i,j], slice_id: item.id, length: length
+          end
+        end
+      end
+    end
+
+    show {
+      title "Cut gel slices and place them in new 1.5 mL tubes"
+      table [
+        [ "Row", "Column", "New Gel Slice id", "Length" ]
+      ].concat( new_routes.collect { |r| [ r[:lane ][0]+1, r[:lane][1]+1, { content: r[:slice_id], check: true }, r[:length] ] } )
+      note "Label the tubes with the id shown above"
+      warning "Wear blue light goggles during this process"
+    }
+	  return items
+  end #gel_band_verify_cut
 
   def main
     io_hash = input[:io_hash]
@@ -57,20 +94,19 @@ class Protocol
   			note "Rename the picture you just took as gel_#{gel.id}. Upload it!"
   			upload var: "my_gel_pic"
   		}
-  		band_mockup = gel_band_verify( gel, except: [ [0,0], [1,0] ] )
-  		gel_data = show {
-  			title "How's the gel size?"
-  			select ["Yes", "No"], var: "okay", label: "Does this gel look as expected?"
-  		}
-  		if gel_data[:okay] == "Yes"
-			s = distribute( gel, "Gel Slice", except: [ [0,0], [1,0] ], interactive: true ) {
-				title "Cut gel slices and place them in new 1.5 mL tubes"
-				note "Label the tubes with the id shown above"
-				warning "Wear blue light goggles during this process"
-			}
+  		s = gel_band_verify_cut( gel, except: [ [0,0], [1,0] ] )
+  		# gel_data = show {
+  		# 	title "How's the gel size?"
+  		# 	select ["Yes", "No"], var: "okay", label: "Does this gel look as expected?"
+  		# }
+
+			# s = distribute( gel, "Gel Slice", except: [ [0,0], [1,0] ], interactive: true ) {
+			# 	title "Cut gel slices and place them in new 1.5 mL tubes"
+			# 	note "Label the tubes with the id shown above"
+			# 	warning "Wear blue light goggles during this process"
+			# }
 			produce s
 			slices = slices.concat s
-		end
   	end
 
   	show {
@@ -80,10 +116,10 @@ class Protocol
   		note "Remove the blue light goggles and put them back where you found them."
   		note "Clean up the gel box and casting tray by rinsing with water. Return them to the gel station."
   	}
-    
-    gels.each do |gel|
-      gel.mark_as_deleted
-    end
+
+    # gels.each do |gel|
+    #   gel.mark_as_deleted
+    # end
 
   	release slices, interactive: true, method: "boxes"
     io_hash[:gel_slice_ids] = slices.collect {|s| s.id}
---
+++ workflows/ecoli/035_gel_purify.rb
@@ -0,0 +1,141 @@
+#This protocol purifies gel slices into fragment stocks
+#Still need to alter the protocol to take a hash as input and output a hash
+#Also need to modify the protocol to be able to run indepently of a metacol
+
+needs "aqualib/lib/standard"
+needs "aqualib/lib/cloning"
+
+class Protocol
+	
+include Standard
+include Cloning
+
+def arguments
+	{
+		io_hash: {},
+		gel_slice_ids: [28515,28516,28517,28510,28511,28512,28513,28514,28515,28507,28508],
+		debug_mode: "Yes"
+	}
+end
+
+def main
+	io_hash = input[:io_hash]
+	io_hash = input if input[:io_hash].empty?
+	slices= io_hash[:gel_slice_ids]
+
+	if io_hash[:debug_mode] == "Yes"
+      def debug
+        true
+      end
+    end
+
+	gel_slices = find(:item, id: slices)
+	num = gel_slices.length
+	num_arr = *(1..num)
+
+	show {
+		title "This protocol purfies gel slices into DNA fragment stocks."
+		note "The following gel slices are going to be purfied"
+		note "#{gel_slices.collect {|s| s.id}}"
+	}
+	
+	take gel_slices, interactive: true,  method: "boxes"
+
+	weights = show{
+		title "Weigh each gel slice."
+		check "Zero the scale"
+		check "Weigh each slice and enter the recorded weights in the following:"
+		gel_slices.each do |gs|
+		  get "number", var: "w#{gs.id}", label: "Enter a number for tube #{gs.id}", default: 0.123
+		end
+	}
+
+	gel_weights = gel_slices.collect { |gs| weights[:"w#{gs.id}".to_sym] }
+
+	qg_volumes = gel_weights.collect { |g| (g*3000).floor}
+	
+	show{
+		# s=*(1..slice_number)
+		title "Add the following volumes of QG buffer to the corresponding tube."
+		table [["Gel Slices", "QG Volume in µl"]].concat(gel_slices.collect {|s| s.id}.zip qg_volumes)
+	}
+	
+	show{
+		title "Place all tubes in 50 degree heat block"
+		timer initial: { hours: 0, minutes: 10, seconds: 0}
+		note "Vortex every few minutes to speed up the process."
+		note "Retreve after 10 minutues or until the gel slice is competely dissovled."
+		note "Add 1x volume (1 uL to 1 mg of gel slice) isopropanol. Pipette up and down to mix"
+	}
+	
+	show {
+		  title "Check the boxes as you complete each step."
+		  check "Grab #{num} of pink Qiagen columns, label with 1 to #{num} on the top."
+		  check "Add tube contents to LABELED pink Qiagen columns using the following table."
+		  check "Be sure not to add more than 750 µl to each pick columns"
+		  table [["Gel slices tube", "Qiagen column"]].concat(gel_slices.collect {|s| s.id}.zip num_arr)
+		}
+		  
+    show {
+    	title "Centrifuge"
+		check "Spin at top speed (> 17,900 g) for 1 minute to bind DNA to columns"
+		check "Empty collection columns by pouring waste liquid into waste liquid container."
+		check "Add 750 µl PE buffer to columns and wait five minutes"
+		check "Spin at top speed (> 17,900 g) for 30 seconds to wash columns."
+		check "Empty collection tubes."
+		check "Add 500 µl PE buffer to columns and wait five minutes"
+		check "Spin at top speed (> 17,900 g) for 30 seconds to wash columns"
+		check "Empty collection tubes."
+		check "Spin at top speed (> 17,900 g) for 1 minute to remove all PE buffer from columns"
+	}
+
+	fragment_stocks = gel_slices.collect {|gs| produce new_sample gs.sample.name, of: "Fragment", as: "Fragment Stock"}
+
+	show{
+		  title "Transfer to 1.5 mL tube"
+		  check "Label #{num} 1.5 mL Eppendorf tube with #{fragment_stocks.collect {|fs| fs.id}}"
+		  check "Transfer pink columns to empty Eppendorf tubes using the following table."
+		  table [["Qiagen column","1.5 mL tube"]].concat(num_arr.zip fragment_stocks.collect {|fs| fs.id})
+		  check "Add 30 µl molecular grade water or EB elution buffer to center of column."
+		  note "Be very careful to not pipette on the wall of the tube"
+		  check "Wait one minute"
+		  timer initial: { hours: 0, minutes: 1, seconds: 0}
+		  check "Elute DNA into Eppendorf tubes by spinning at top speed (> 17,900 xg) for one minute"
+	}
+	
+	concs = show{
+		title "Measure DNA Concentration"
+		note "Go to B9 and nanodrop all of 1.5 mL tubes, enter DNA concentrations for all tubes in the following:"
+		fragment_stocks.each do |fs|
+		  get "number", var: "c#{fs.id}", label: "Enter a number for tube #{fs.id}", default: 30.2
+		end
+	}
+
+	# show {
+	# fragment_stocks.each do |fs|
+	# 	fs.datum = {}
+	# 	note "#{concs[:"c#{fs.id}".to_sym]}"
+	# 	fs.save
+	# end
+	# }
+
+	#
+
+	fragment_stocks.each do |fs|
+		fs.datum = {"concentration"=> concs[:"c#{fs.id}".to_sym],"volume"=> 28}
+		fs.save
+	end
+
+	# gel_slices.each do |gs|
+	# 	gs.mark_as_deleted
+	# end
+
+	release fragment_stocks, interactive: true, method: "boxes"
+	io_hash[:fragment_stock_ids] = fragment_stocks.collect{|fs| fs.id}
+	return {io_hash: io_hash}
+
+	end
+
+end
+
+
