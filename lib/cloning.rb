ac39528db91e2428d8637fe3cef553f526c63c6b
sunshineyyy <yaoyu.yang88@gmail.com>
2014-10-15 22:16:33 -0700

    improving gibson_assembly_status

--- lib/cloning.rb
+++ lib/cloning.rb
@@ -81,7 +81,7 @@ module Cloning
         info = fragment_info fid
 
         # First check if there already exists fragment stock, if so, it's ready to build.
-        if find(:sample, id: fid).in("Fragment Stock")
+        if find(:sample, id: fid)[0].in("Fragment Stock")
           t[:fragments][:ready_to_use].push fid
         elsif !info
           t[:fragments][:not_ready_to_build].push fid
