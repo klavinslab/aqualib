548f6ed157cbbacae4d9aac13a884d8517841978
sunshineyyy <yaoyu.yang88@gmail.com>
2014-10-26 03:14:31 -0700

    work on plasmid_construction workflow, enable task save on plate status

--- workflows/ecoli/052_plate_ecoli_transformation.rb
+++ workflows/ecoli/052_plate_ecoli_transformation.rb
@@ -24,14 +24,14 @@ class Protocol
       end
     end
     all_transformed_aliquots = io_hash[:transformed_aliquots_ids].collect { |tid| find(:item, id: tid)[0] }
-    if all_transformed_aliquots.length == 0
-      show {
-        title "No plating required"
-        note "No transformed aliquots need to be plated."
-      }
-      return {io_hash: io_hash}
-    end
-    take all_transformed_aliquots, interactive: true
+    # if all_transformed_aliquots.length == 0
+    #   show {
+    #     title "No plating required"
+    #     note "No transformed aliquots need to be plated."
+    #   }
+    #   return {io_hash: io_hash}
+    # end
+    take all_transformed_aliquots, interactive: true if all_transformed_aliquots.length > 0
 
     transformed_aliquot_marker_hash = Hash.new { |h,k| h[k] = [] }
     all_transformed_aliquots.each do |t|
@@ -74,7 +74,7 @@ class Protocol
   		p.save
   	end
 
-  	release all_plates, interactive: true
+  	release all_plates, interactive: true, if all_plates.length > 0
   	io_hash[:plate_ids] = [] if !io_hash[:plate_ids]
   	io_hash[:plate_ids].concat all_plates.collect { |p| p.id }
 
