32ef18972bb90b5c4f45c5cf822b291fd668045d
sunshineyyy <yaoyu.yang88@gmail.com>
2014-10-28 11:09:03 -0700

    update g418

--- workflows/yeast/make_G418_plates.rb
+++ workflows/yeast/make_G418_plates.rb
@@ -17,15 +17,22 @@ class Protocol
   def main
     io_hash = input[:io_hash]
     io_hash = input if !input[:io_hash] || input[:io_hash].empty?
-    x = debug_mode true 
-    show {
-      note "#{x}"
-    }
+    if io_hash[:debug_mode].downcase == "yes"
+      def debug
+        true
+      end
+    end
+    plasmid_kan_ids = io_hash[:plasmid_ids].select { |pid| find(:item, id: pid)[0].sample.properties["Yeast Marker"].downcase[0,3]== "kan"}
+    num = plasmid_kan_ids.length
     show {
     	title "Grab YPAD plates and G418 stock"
+      check "Grab #{num} YPAD plates."
+      check "Grab #{(num*300/1000.0).ceil} 1 mL G418 stock in M20."
+      check "Waiting for the G418 stock to thaw."
+      check "Use sterile beads to spread 300 µL of G418 to each YPAD plates, mark each plate with G418."
+      check "Place the plates with agar side down in the dark fume hood to dry."
     }
-
-
+    return { io_hash: io_hash }
   end # main
 
 end # Protocol
