80158789eecca32044ba506571a5d97d0df47a99
sunshineyyy <yaoyu.yang88@gmail.com>
2014-10-29 12:52:41 -0700

    fix bug in dilute plasmid stock that produces multiple 1ng/ul stock

--- workflows/ecoli/009_dilute_plasmid_stock.rb
+++ workflows/ecoli/009_dilute_plasmid_stock.rb
@@ -37,7 +37,8 @@ class Protocol
       fragment_ids.concat task.simple_spec[:fragments]
     end
     plasmids = fragment_ids.collect{|f| find(:sample, id: f)[0].properties["Template"]}
-    plasmids = plasmids.compact
+    # remove redundant plasmids 
+    plasmids = plasmids.compact.uniq
     plasmids_need_to_dilute = plasmids.select{ |p| p.in("Plasmid Stock").length > 0 && (p.in("1 ng/µL Plasmid Stock").length == 0) }
     plasmid_stocks = plasmids_need_to_dilute.collect{ |p| p.in("Plasmid Stock")[0] }
     # concat with input to this protocol if input[:plasmid_stock_ids] is defined
