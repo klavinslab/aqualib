d97b71243a7f691c9a6b1174752b4525fb63a16c
sunshineyyy <yaoyu.yang88@gmail.com>
2014-10-30 11:20:39 -0700

    update gel purify timer

--- workflows/ecoli/035_gel_purify.rb
+++ workflows/ecoli/035_gel_purify.rb
@@ -1,4 +1,4 @@
-#First version by Miles Gander, refactored by Yaoyu Yang.
+# First version by Miles Gander, refactored by Yaoyu Yang.
 # To do list
 # Group into 12 things one page
 needs "aqualib/lib/standard"
@@ -12,7 +12,7 @@ class Protocol
 	def arguments
 		{
 			io_hash: {},
-			gel_slice_ids: [28515,28516,28517,28510,28511,28512,28513,28514,28515,28507,28508],
+			gel_slice_ids: [],
 			debug_mode: "Yes"
 		}
 	end
@@ -35,14 +35,15 @@ class Protocol
 		num_arr = *(1..num)
 
 		show {
-			title "This protocol purfies gel slices into DNA fragment stocks."
+			title "Protocol Information"
+			note "This protocol purfies gel slices into DNA fragment stocks."
 			note "The following gel slices are going to be purfied"
 			note "#{gel_slices.collect {|s| s.id}}"
 		}
 
 		take gel_slices, interactive: true,  method: "boxes"
 
-		weights = show{
+		weights = show {
 			title "Weigh each gel slice."
 			check "Zero the scale"
 			check "Weigh each slice and enter the recorded weights in the following:"
@@ -63,94 +64,87 @@ class Protocol
 			end
 		end
 
-
 		show{
-		# s=*(1..slice_number)
-		title "Add the following volumes of QG buffer to the corresponding tube."
-		table [["Gel Slices", "QG Volume in µl"]].concat(gel_slices.collect {|s| s.id}.zip qg_volumes)
-	}
+			title "Add the following volumes of QG buffer to the corresponding tube."
+			table [["Gel Slices", "QG Volume in µl"]].concat(gel_slices.collect {|s| s.id}.zip qg_volumes)
+	  }
 	
-	show{
-		title "Place all tubes in 50 degree heat block"
-		timer initial: { hours: 0, minutes: 10, seconds: 0}
-		note "Vortex every few minutes to speed up the process."
-		note "Retreve after 10 minutues or until the gel slice is competely dissovled."
-		note "Add isopropanol according to the following table. Pipette up and down to mix"
-		table [["Gel slice", "Isopropanol"]].concat(gel_slices.collect {|s| s.id}.zip iso_volumes)
-	}
-	
-	show {
-		title "Check the boxes as you complete each step."
-		check "Grab #{num} of pink Qiagen columns, label with 1 to #{num} on the top."
-		check "Add tube contents to LABELED pink Qiagen columns using the following table."
-		check "Be sure not to add more than 750 µL to each pick columns"
-		table [["Gel slices tube", "Qiagen column"]].concat(gel_slices.collect {|s| s.id}.zip num_arr)
-	}
+		show {
+			title "Place all tubes in 50 degree heat block"
+			timer initial: { hours: 0, minutes: 10, seconds: 0}
+			note "Vortex every few minutes to speed up the process."
+			note "Retreve after 10 minutues or until the gel slice is competely dissovled."
+		}
 
-	show {
-		title "Centrifuge"
-		check "Spin at top speed (> 17,900 g) for 1 minute to bind DNA to columns"
-		check "Empty collection columns by pouring waste liquid into waste liquid container."
-		check "Add 750 µL PE buffer to columns and wait five minutes"
-		check "Spin at top speed (> 17,900 g) for 30 seconds to wash columns."
-		check "Empty collection tubes."
-		check "Add 500 µL PE buffer to columns and wait five minutes"
-		check "Spin at top speed (> 17,900 g) for 30 seconds to wash columns"
-		check "Empty collection tubes."
-		check "Spin at top speed (> 17,900 g) for 1 minute to remove all PE buffer from columns"
-	}
+		show {
+			note "Add isopropanol according to the following table. Pipette up and down to mix"
+			table [["Gel slice", "Isopropanol"]].concat(gel_slices.collect {|s| s.id}.zip iso_volumes)
+		} if (iso_volumes.select { |v| v > 0 }).length > 0
 
-	fragment_stocks = gel_slices.collect {|gs| produce new_sample gs.sample.name, of: "Fragment", as: "Fragment Stock"}
+		show {
+			title "Check the boxes as you complete each step."
+			check "Grab #{num} of pink Qiagen columns, label with 1 to #{num} on the top."
+			check "Add tube contents to LABELED pink Qiagen columns using the following table."
+			check "Be sure not to add more than 750 µL to each pick columns"
+			table [["Gel slices tube", "Qiagen column"]].concat(gel_slices.collect {|s| s.id}.zip num_arr)
+		}
 
-	show {
-		title "Transfer to 1.5 mL tube"
-		check "Label #{num} 1.5 mL tube with #{fragment_stocks.collect {|fs| fs.id}}"
-		check "Transfer pink columns to empty Eppendorf tubes using the following table."
-		table [["Qiagen column","1.5 mL tube"]].concat(num_arr.zip fragment_stocks.collect {|fs| fs.id})
-		check "Add 30 µl molecular grade water or EB elution buffer to center of column."
-		note "Be very careful to not pipette on the wall of the tube"
-	}
+		show {
+			title "Centrifuge"
+			check "Spin at top speed (> 17,900 g) for 1 minute to bind DNA to columns"
+			check "Empty collection columns by pouring waste liquid into waste liquid container."
+			check "Add 750 µL PE buffer to columns and wait five minutes"
+			check "Spin at top speed (> 17,900 g) for 30 seconds to wash columns."
+			check "Empty collection tubes."
+			check "Add 500 µL PE buffer to columns and wait five minutes"
+			check "Spin at top speed (> 17,900 g) for 30 seconds to wash columns"
+			check "Empty collection tubes."
+			check "Spin at top speed (> 17,900 g) for 1 minute to remove all PE buffer from columns"
+		}
 
-	show {
-		title "Wait one minute and then spin"
-		timer initial: { hours: 0, minutes: 1, seconds: 0}
-		check "Elute DNA into Eppendorf tubes by spinning at top speed (> 17,900 xg) for one minute"
-	}
-	
-	concs = show{
-		title "Measure DNA Concentration"
-		note "Go to B9 and nanodrop all of 1.5 mL tubes, enter DNA concentrations for all tubes in the following:"
+		fragment_stocks = gel_slices.collect {|gs| produce new_sample gs.sample.name, of: "Fragment", as: "Fragment Stock"}
+
+		show {
+			title "Transfer to 1.5 mL tube"
+			check "Label #{num} 1.5 mL tube with #{fragment_stocks.collect {|fs| fs.id}}"
+			check "Transfer pink columns to empty Eppendorf tubes using the following table."
+			table [["Qiagen column","1.5 mL tube"]].concat(num_arr.zip fragment_stocks.collect {|fs| fs.id})
+			check "Add 30 µL molecular grade water or EB elution buffer to center of the column."
+			warning "Be very careful to not pipette on the wall of the tube."
+		}
+
+		show {
+			title "Wait one minute"
+			timer initial: { hours: 0, minutes: 1, seconds: 0}
+		}
+		
+		concs = show {
+			title "Measure DNA Concentration"
+			check "Elute DNA into 1.5 mL tubes by spinning at top speed (> 17,900 xg) for one minute, discard the columns."
+			check "Go to B9 and nanodrop all of 1.5 mL tubes, enter DNA concentrations for all tubes in the following:"
+			fragment_stocks.each do |fs|
+				get "number", var: "c#{fs.id}", label: "Enter a number for tube #{fs.id}", default: 30.2
+			end
+		}
+
 		fragment_stocks.each do |fs|
-			get "number", var: "c#{fs.id}", label: "Enter a number for tube #{fs.id}", default: 30.2
+			fs.datum = {concentration: concs[:"c#{fs.id}".to_sym], volume: 28}
+			fs.save
 		end
-	}
 
-	# show {
-	# fragment_stocks.each do |fs|
-	# 	fs.datum = {}
-	# 	note "#{concs[:"c#{fs.id}".to_sym]}"
-	# 	fs.save
-	# end
-	# }
+		gel_slices.each do |gs|
+			gs.mark_as_deleted
+		end
 
-	fragment_stocks.each do |fs|
-		fs.datum = {concentration: concs[:"c#{fs.id}".to_sym], volume: 28}
-		fs.save
-	end
+		# run gibson_assembly_status to update all the tasks status.
+		gas = gibson_assembly_status
 
-	gel_slices.each do |gs|
-		gs.mark_as_deleted
-	end
+		release fragment_stocks, interactive: true, method: "boxes"
+		io_hash[:fragment_stock_ids] = fragment_stocks.collect{|fs| fs.id}
+		return {io_hash: io_hash}
 
-	# run gibson_assembly_status to update all the tasks status.
-	gas = gibson_assembly_status
+  end # main
 
-	release fragment_stocks, interactive: true, method: "boxes"
-	io_hash[:fragment_stock_ids] = fragment_stocks.collect{|fs| fs.id}
-	return {io_hash: io_hash}
-
-end
-
-end
+end # Protocol
 
 
--- workflows/ecoli/052_plate_ecoli_transformation.rb
+++ workflows/ecoli/052_plate_ecoli_transformation.rb
@@ -24,13 +24,12 @@ class Protocol
       end
     end
     all_transformed_aliquots = io_hash[:transformed_aliquots_ids].collect { |tid| find(:item, id: tid)[0] }
-    # if all_transformed_aliquots.length == 0
-    #   show {
-    #     title "No plating required"
-    #     note "No transformed aliquots need to be plated."
-    #   }
-    #   return {io_hash: io_hash}
-    # end
+    if all_transformed_aliquots.length == 0
+      show {
+        title "No plating required"
+        note "No transformed aliquots need to be plated. Thanks for your effort!"
+      }
+    end
     take all_transformed_aliquots, interactive: true if all_transformed_aliquots.length > 0
 
     transformed_aliquot_marker_hash = Hash.new { |h,k| h[k] = [] }
