5869e95d59ba85484717517b314558a4d05bb89e
sunshineyyy <yaoyu.yang88@gmail.com>
2014-11-09 17:55:41 -0800

    work on tables for streak_yeast_colonies.rb

--- workflows/yeast/055_streak_yeast_colonies.rb
+++ workflows/yeast/055_streak_yeast_colonies.rb
@@ -28,12 +28,21 @@ class Protocol
 
     yeast_plates = io_hash[:yeast_plate_ids].collect { |yid| find(:item, id: yid)[0] }
     take yeast_plates, interactive: true
-    show {
-      note "#{yeast_plates.collect { |y| y.id }}"
-    }
     streaked_yeast_plates = yeast_plates.collect { |y| produce new_sample y.sample.name, of: "Yeast Strain", as: "Yeast Plate"}
+    num = streaked_yeast_plates.length
     show {
-      note "#{streaked_yeast_plates.collect { |y| y.id }}"
+      title "Grab YPAD plates"
+      note "Grab #{num} of YPAD plates, label with follow ids"
+      note streaked_yeast_plates.collect { |p| "#{p}"}
+    }
+    tab = [["Yeast plates","New streaked yeast plates","num_colonies"]]
+    yeast_plates.each_with_index do |yp,idx|
+      tab.push(["#{yp}","#{streaked_yeast_plates[idx]}",{ content: io_hash[:num_colonies][idx], check: true }])
+    end
+    show {
+      title "Streak yeast colonies on new YPAD plates"
+      note "Streak using the following table, divide up the plates into num_colonies regions when streaking"
+      table tab
     }
     streaked_yeast_plates.each_with_index do |y,idx|
       y.datum = { from: yeast_plates[idx].id, regions: io_hash[:num_colonies][idx] }
