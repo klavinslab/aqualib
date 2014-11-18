7e75a773cc61174475be0272ba9cf85b182beb9a
Eric Klavins <klavins@uw.edu>
2014-11-17 20:33:19 -0800

    debug

--- plugins/activity_monitor/interface.rb
+++ plugins/activity_monitor/interface.rb
@@ -1,9 +1,11 @@
 class PluginInterface < PluginInterfaceBase
 
   def data params
+
     @view.logger.info "AAAAAA"
     now = Time.now
     since = Time.at(params[:since])
+
     return { 
       start: since,
       timestamp: now,
@@ -11,8 +13,10 @@ class PluginInterface < PluginInterfaceBase
         .collect { |j| { updated_at: j.updated_at, id: j.id } },
       samples: Sample.where("created_at >= ? AND created_at < ?", since, now)
         .collect { |s| { updated_at: s.updated_at, id: s.id } },
-
     }
+
+    @view.logger.info "BBBBBBB"
+
   end
 
 end
