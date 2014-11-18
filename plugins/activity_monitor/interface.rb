34d38a9c5f7f46dcecff4bf3d6a621e57e5362b4
Eric Klavins <klavins@uw.edu>
2014-11-17 20:53:32 -0800

    rearrange

--- plugins/activity_monitor/interface.rb
+++ plugins/activity_monitor/interface.rb
@@ -5,13 +5,15 @@ class PluginInterface < PluginInterfaceBase
     now = Time.now
     since = Time.at(params[:since])
 
+    s = Sample.where("created_at >= ? AND created_at < ?", since, now)
+        .collect { |s| { updated_at: s.created_at, id: s.id } }
+
     result = { 
       start: since,
       timestamp: now,
       jobs: Job.where("updated_at >= ? AND updated_at < ? AND pc = -2", since, now)
         .collect { |j| { updated_at: j.updated_at, id: j.id } },
-      samples: Sample.where("created_at >= ? AND created_at < ?", since, now)
-        .collect { |s| { updated_at: s.created_at, id: s.id } }
+      samples: s
     }
 
     return result
