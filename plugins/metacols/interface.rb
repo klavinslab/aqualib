751c76c8853ba6cd386d746d0bd1d066aa59da26
Eric Klavins <klavins@uw.edu>
2014-11-17 20:10:21 -0800

    attr_writer

--- plugins/metacols/interface.rb
+++ plugins/metacols/interface.rb
@@ -3,8 +3,8 @@ class PluginInterface < PluginInterfaceBase
   def data params
     metacols = Metacol.includes(:user).where(status: "RUNNING")
     metacols.each do |m|
-      m[:login] = m.user.login
-      m[:date] = (view.time_ago_in_words m.updated_at) + " ago"
+      m.login = m.user.login
+      m.date = (view.time_ago_in_words m.updated_at) + " ago"
     end
     return { metacols: metacols, current_user: @view.current_user }
   end
