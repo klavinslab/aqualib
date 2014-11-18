314f83124eb943b720cc118f53403c814311ede7
sunshineyyy <yaoyu.yang88@gmail.com>
2014-11-18 12:52:53 -0800

    add length info, ready to build and not ready to build

--- workflows/test/check_tasks_inputs.rb
+++ workflows/test/check_tasks_inputs.rb
@@ -9,29 +9,29 @@ class Protocol
   def main
 
     gs = gibson_assembly_status
-    waiting_ids = gs[:waiting_ids]
-    users = waiting_ids.collect { |tid| find(:task, id: tid)[0].user.name }
-    fragment_ids = waiting_ids.collect { |tid| find(:task, id: tid)[0].simple_spec[:fragments] }
-    plasmid_ids = waiting_ids.collect { |tid| find(:task, id: tid)[0].simple_spec[:plasmid] }
-    plasmids = plasmid_ids.collect { |pid| find(:sample, id: pid)[0]}
-    # fragments = fragment_ids.collect { |fids| fids.collect { |fid| "#{find(:sample, id: fid)[0]}" }[0] }
-    # show {
-    #   note "#{fragment_ids}"
-    # }
-    gs_tab = [[ "List of not ready tasks", "Tasks owner", "Fragments", "Plasmid" ]]
-    waiting_ids.each_with_index do |tid,idx|
-      gs_tab.push [ tid, users[idx], fragment_ids[idx].to_s, "#{plasmids[idx]}"]
-    end
-    show {
-      title "Gibson Assemby Status"
-      table gs_tab
-      if gs[:fragments]
-        note "Ready to use fragment ids: #{gs[:fragments][:ready_to_use].uniq}"
-        note "Not ready to use fragment ids, probably missing length info: #{gs[:fragments][:not_ready_to_use].uniq}"
-        note "Ready to build fragment ids: #{gs[:fragments][:ready_to_build].uniq}" 
-        note "Not ready to build fragment ids: #{gs[:fragments][:not_ready_to_build].uniq}"
+    if gs[:fragments]
+      waiting_ids = gs[:waiting_ids]
+      users = waiting_ids.collect { |tid| find(:task, id: tid)[0].user.name }
+      fragment_ids = waiting_ids.collect { |tid| find(:task, id: tid)[0].simple_spec[:fragments] }
+      ready_to_use_fragment_ids = gs[:fragments][:ready_to_use].uniq
+      not_ready_to_use_fragment_ids = gs[:fragments][:not_ready_to_use].uniq
+      ready_to_build_fragment_ids = gs[:fragments][:ready_to_build].uniq
+      not_ready_to_build_fragment_ids = gs[:fragments][:not_ready_to_build].uniq
+      plasmid_ids = waiting_ids.collect { |tid| find(:task, id: tid)[0].simple_spec[:plasmid] }
+      plasmids = plasmid_ids.collect { |pid| find(:sample, id: pid)[0]}
+      # fragments = fragment_ids.collect { |fids| fids.collect { |fid| "#{find(:sample, id: fid)[0]}" }[0] }
+      # show {
+      #   note "#{fragment_ids}"
+      # }
+      gs_tab = [[ "Not ready tasks", "Tasks owner", "Plasmid", "Fragments", "Length info missing", "Ready to build", "Not ready to build" ]]
+      waiting_ids.each_with_index do |tid,idx|
+        gs_tab.push [ tid, users[idx], "#{plasmids[idx]}", fragment_ids[idx].to_s, (fragment_ids[idx]&not_ready_to_use_fragment_ids).to_s, (fragment_ids[idx]&ready_to_build_fragment_ids).to_s, (fragment_ids[idx]&not_ready_to_build_fragment_ids).to_s ]
       end
-    }
+      show {
+        title "Gibson Assemby Status"
+        table gs_tab
+      }
+    end
 
   end # main
 
