f8b071c778ea0b434b9f0f29d39b3912cb496322
Eric Klavins <klavins@uw.edu>
2014-11-17 20:56:41 -0800

    asd

--- plugins/activity_monitor/interface.rb
+++ plugins/activity_monitor/interface.rb
@@ -5,15 +5,13 @@ class PluginInterface < PluginInterfaceBase
     now = Time.now
     since = Time.at(params[:since])
 
-    s = Sample.where("created_at >= ? AND created_at < ?", since, now)
-        .collect { |s| { updated_at: s.created_at, id: s.id } }
-
     result = { 
       start: since,
       timestamp: now,
       jobs: Job.where("updated_at >= ? AND updated_at < ? AND pc = -2", since, now)
         .collect { |j| { updated_at: j.updated_at, id: j.id } },
-      samples: s
+      samples: Sample.where("created_at >= ? AND created_at < ?", since, now)
+        .collect { |s| { updated_at: s.created_at, id: s.id } }
     }
 
     return result
--- plugins/metacols/interface.rb
+++ plugins/metacols/interface.rb
@@ -1,10 +1,12 @@
 class PluginInterface < PluginInterfaceBase
 
   def data params
-    metacols = Metacol.includes(:user).where(status: "RUNNING")
+
+    metacols = Metacol.includes(:user).where(status: "RUNNING").collect { |m| m.attributes }
+
     metacols.each do |m|
-      m.login = m.user.login
-      m.date = (view.time_ago_in_words m.updated_at) + " ago"
+      m[:login] = m.user.login
+      m[:date] = (view.time_ago_in_words m.updated_at) + " ago"
     end
     return { metacols: metacols, current_user: @view.current_user }
   end
