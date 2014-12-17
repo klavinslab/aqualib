434b5b27eb5a88565597452050bbfba3e1d5c5c8
Eric Klavins <klavins@uw.edu>
2014-12-17 15:41:44 -0800

    another update test

--- sandbox/klavins/infinite_debug_loop.rb
+++ sandbox/klavins/infinite_debug_loop.rb
@@ -1,5 +1,6 @@
 #
-# This protocol used to crash the Krill server, but now it won't.
+# This protocol used to crash the Krill server, but now it won't because
+# Krill now has a limit to how many shows can be requested before stopping.
 #
 
 class Protocol
