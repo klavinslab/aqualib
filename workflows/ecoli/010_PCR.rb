f6e45376cd87ea54c5ec8cfb583173f86813da86
sunshineyyy <yaoyu.yang88@gmail.com>
2015-01-13 15:49:14 -0800

    update

--- workflows/ecoli/010_PCR.rb
+++ workflows/ecoli/010_PCR.rb
@@ -41,7 +41,7 @@ class Protocol
     # Pull info from Gibson Assembly Tasks
     gibson_assembly = gibson_assembly_status
     fragment_from_gibson_ids = []
-    fragment_from_gibson_ids = gibson_assembly[:fragments][:ready_to_build] if gibson_assembly[:fragments]
+    #fragment_from_gibson_ids = gibson_assembly[:fragments][:ready_to_build] if gibson_assembly[:fragments]
     
     # Pull info from Fragment Construction Tasks
     fragment_construction = fragment_construction_status
