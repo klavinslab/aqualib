66e8cee187b20d8436d50f8bec1ecf172e9fb129
sunshineyyy <yaoyu.yang88@gmail.com>
2015-01-19 22:39:38 -0800

    work on updating yeast transformation related protocols

--- workflows/yeast/050_plate_yeast_transformation.rb
+++ workflows/yeast/050_plate_yeast_transformation.rb
@@ -24,53 +24,59 @@ class Protocol
     end
 
     yeast_transformation_mixtures = io_hash[:yeast_transformation_mixture_ids].collect {|tid| find(:item, id: tid )[0]}
-    take yeast_transformation_mixtures, interactive: true
-    
-    show {
-      title "Resuspend in water"
-      check "Spin down all the tubes in a small table top centrifuge for ~1 minute"
-      check "Pipette off supernatant being careful not to disturb yeast pellet"
-      check "Add 600 µL of sterile water to each eppendorf tube"
-      check "Resuspend the pellet by vortexing the tube throughly"
-      warning "Make sure the pellet is resuspended and there are no cells stuck to the bottom of the tube"
-    }
 
-    yeast_plates = yeast_transformation_mixtures.collect {|y| produce new_sample y.sample.name, of: "Yeast Strain", as: "Yeast Plate"}
+    if yeast_transformation_mixtures.length == 0
+      show {
+        title "No plating required"
+        note "No transformed aliquots need to be plated. Thanks for your effort!"
+      }
+    else
+      take yeast_transformation_mixtures, interactive: true
+      
+      show {
+        title "Resuspend in water"
+        check "Spin down all the tubes in a small table top centrifuge for ~1 minute"
+        check "Pipette off supernatant being careful not to disturb yeast pellet"
+        check "Add 600 µL of sterile water to each eppendorf tube"
+        check "Resuspend the pellet by vortexing the tube throughly"
+        warning "Make sure the pellet is resuspended and there are no cells stuck to the bottom of the tube"
+      }
 
-    move yeast_plates, "30 C incubator"
+      yeast_plates = yeast_transformation_mixtures.collect {|y| produce new_sample y.sample.name, of: "Yeast Strain", as: "Yeast Plate"}
 
-    tab = [["Yeast Transformation Mixtures id","Plate id"]]
-    yeast_transformation_mixtures.each_with_index do |y,idx|
-      tab.push([y.id,yeast_plates[idx].id])
-    end
+      move yeast_plates, "30 C incubator"
 
-    show {
-      title "Plating"
-      check "Grab #{yeast_plates.length} +G418 plates. Label plates with the following ids"
-      note (yeast_plates.collect{|y| "#{y}"})
-      check "Flip the plate and add 4-5 glass beads to it"
-      check "Add 200 µL of the transformation mixture from the tube according to the following table"
-      table [["Yeast Transformation Mixtures id","Plate id"]].concat(yeast_transformation_mixtures.collect { |y| y.id }.zip yeast_plates.collect { |y| { content: y.id, check: true } })
-    }
-
-    show {
-      title "Shake and incubate"
-      check "Shake the plates in all directions to evenly spread the culture over its surface till dry"
-      check "Discard the beads in a used beads container"
-      check "Throw away transformation mixture tubes"
-      check "Put the plates with the agar side up in the 30C incubator"
-    }
-    
-    release yeast_plates, interactive: true
-    delete yeast_transformation_mixtures
-
-    if io_hash[:task_ids]
-      io_hash[:task_ids].each do |tid|
-        task = find(:task, id: tid)[0]
-        set_task_status(task,"plated")
+      tab = [["Yeast Transformation Mixtures id","Plate id"]]
+      yeast_transformation_mixtures.each_with_index do |y,idx|
+        tab.push([y.id,yeast_plates[idx].id])
       end
+
+      show {
+        title "Plating"
+        check "Grab #{yeast_plates.length} +G418 plates. Label plates with the following ids"
+        note (yeast_plates.collect{|y| "#{y}"})
+        check "Flip the plate and add 4-5 glass beads to it"
+        check "Add 200 µL of the transformation mixture from the tube according to the following table"
+        table [["Yeast Transformation Mixtures id","Plate id"]].concat(yeast_transformation_mixtures.collect { |y| y.id }.zip yeast_plates.collect { |y| { content: y.id, check: true } })
+      }
+
+      show {
+        title "Shake and incubate"
+        check "Shake the plates in all directions to evenly spread the culture over its surface till dry"
+        check "Discard the beads in a used beads container"
+        check "Throw away transformation mixture tubes"
+        check "Put the plates with the agar side up in the 30C incubator"
+      }
+      
+      release yeast_plates, interactive: true
+      delete yeast_transformation_mixtures
     end
-    io_hash[:plate_ids] = [] if !io_hash[:plate_ids]
+
+    io_hash = ({ plate_ids: [], task_ids: [] }).merge io_hash
+    io_hash[:task_ids].each do |tid|
+      task = find(:task, id: tid)[0]
+      set_task_status(task,"plated")
+    end
     io_hash[:plate_ids].concat yeast_plates.collect { |p| p.id }
     
     return { io_hash: io_hash }
