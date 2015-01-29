7f38493ed82650cd6dca7b99fce7c2ffe8253e57
sunshineyyy <yaoyu.yang88@gmail.com>
2015-01-28 22:22:55 -0800

    update dilute_plasmid_stock also to dilute fragment stock to 1ng/uL

--- workflows/ecoli/009_dilute_plasmid_stock.rb
+++ workflows/ecoli/009_dilute_plasmid_stock.rb
@@ -40,13 +40,13 @@ class Protocol
     plasmids = fragment_ids.collect{|f| find(:sample, id: f)[0].properties["Template"]}
     # remove redundant plasmids 
     plasmids = plasmids.compact.uniq
-    plasmids_need_to_dilute = plasmids.select{ |p| p.in("Plasmid Stock").length > 0 && (p.in("1 ng/µL Plasmid Stock").length == 0) }
-    plasmid_stocks = plasmids_need_to_dilute.collect{ |p| p.in("Plasmid Stock")[0] }
+    plasmids_need_to_dilute = plasmids.select{ |p| p.in(p.sample.sample_type.name + " Stock").length > 0 && p.in("1 ng/µL " + p.sample.sample_type.name + " Stock").length == 0 }
+    plasmid_stocks = plasmids_need_to_dilute.collect{ |p| p.in(p.sample.sample_type.name + " Stock")[0] }
     # concat with input to this protocol if input[:plasmid_stock_ids] is defined
   	plasmid_stocks.concat input[:plasmid_stock_ids].collect{ |fid| find(:item, id: fid)[0] } if input[:plasmid_stock_ids]
     if plasmid_stocks.length == 0
       show {
-        title "No plasmid stocks need to be diluted"
+        title "No plasmid or fragment stocks need to be diluted"
         note "Thanks for you efforts! Please work on the next protocol!"
       }
       io_hash[:plasmid_diluted_stock_ids]  = []
@@ -58,7 +58,7 @@ class Protocol
     plasmid_stocks_need_to_measure = plasmid_stocks.select {|f| !f.datum[:concentration]}
     while plasmid_stocks_need_to_measure.length > 0
       data = show {
-        title "Nanodrop the following plasmid stocks."
+        title "Nanodrop the following plasmid/fragment stocks."
         plasmid_stocks_need_to_measure.each do |ps|
           get "number", var: "c#{ps.id}", label: "Go to B9 and nanodrop tube #{ps.id}, enter DNA concentrations in the following", default: 100
         end
@@ -73,14 +73,14 @@ class Protocol
     concs = plasmid_stocks.collect {|f| f.datum[:concentration].to_f}
   	water_volumes = concs.collect {|c| c-1}
     # produce 1 ng/µL Plasmid Stocks
-    plasmid_diluted_stocks = plasmid_stocks.collect {|f| produce new_sample f.sample.name, of: "Plasmid", as: "1 ng/µL Plasmid Stock"}
+    plasmid_diluted_stocks = plasmid_stocks.collect {|f| produce new_sample f.sample.name, of: f.sample.sample_type.name, as: ("1 ng/µL " + f.sample.sample_type.name + " Stock") }
     # build a checkable table for user
-    tab = [["Newly labled tube","Plasmid stock, 1 µL","Water volume"]]
+    tab = [["Newly labled tube","Plasmid/Fragment stock, 1 µL","Water volume"]]
   	plasmid_stocks.each_with_index do |f,idx|
   		tab.push([plasmid_diluted_stocks[idx].id, { content: f.id, check: true }, { content: water_volumes[idx].to_s + " µL", check: true }])
   	end
   	show {
-  		title "Make 1 ng/µL Plasmid Stocks"
+  		title "Make 1 ng/µL Plasmid/Fragment Stocks"
   		check "Grab #{plasmid_stocks.length} 1.5 mL tubes, label them with #{plasmid_diluted_stocks.collect {|f| f.id}}"
   		check "Add plasmid stocks and water into newly labeled 1.5 mL tubes following the table below"
   		table tab
