6776baa51bfe4b98e7e7cc494f22955f5afce2e2
MilesGander <gandermiles@gmail.com>
2015-02-09 15:27:32 -0800

    Update ConvertObject_SampleType.rb

--- sandbox/Miles/ConvertObject_SampleType.rb
+++ sandbox/Miles/ConvertObject_SampleType.rb
@@ -1,1 +1,29 @@
 class protocol
+  
+  include Standard
+  include Cloning
+  
+  def argumnets
+    #Enter the plasmid stocks ids that you wish to convert to another plasmid 
+    plasmidstock_ids: [],
+    
+    #Enter the corresponding plasmid you would like to convert the plasmid stock too
+    plasmid_ids: [],
+  end
+  
+  def main
+    stocks=input[:plasmidstock_ids]
+    stocks_lengths=stocks.length
+    stocks_full=find(:item, id: stocks)
+    
+    samps=input[:plasmid_ids]
+    samps_full=find(:sample, id: samps)
+  
+    
+    count=0
+    while count < stocks_lengths do
+      stocks_full[count].sample=samps_full[count]
+      count=count+1
+    end
+  end
+  
