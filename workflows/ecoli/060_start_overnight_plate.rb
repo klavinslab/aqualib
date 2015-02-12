11042867da24293f5ae60184234bdcc9c8c9150b
sunshineyyy <yaoyu.yang88@gmail.com>
2015-02-11 16:40:11 -0800

    update overnight

--- workflows/ecoli/060_start_overnight_plate.rb
+++ workflows/ecoli/060_start_overnight_plate.rb
@@ -67,6 +67,9 @@ class Protocol
     num_colonies = []
     primer_ids = []
     initials = []
+    show {
+      note "#{io_hash}"
+    }
     io_hash[:plate_ids].each_with_index do |pid,idx|
       if find(:item, id: pid)[0].sample.properties["Bacterial Marker"] == ""
         info_needed_plate_ids.push pid
