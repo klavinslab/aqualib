f74c4f419c278b3c8d54b78232de9c53282b3cbd
sunshineyyy <yaoyu.yang88@gmail.com>
2015-02-20 17:57:15 -0800

    fix typo

--- lib/cloning.rb
+++ lib/cloning.rb
@@ -429,9 +429,9 @@ module Cloning
         ready_conditions = t[:fragments][:ready_to_build].length == t.simple_spec[:fragments].length
 
       when "Plasmid Verification"
-        length_check = t.simple_spec[:plated_ids].length == t.simple_spec[:num_colonies] && t.simple_spec[:plated_ids].length == t.simple_spec[:primer_ids]
+        length_check = t.simple_spec[:plate_ids].length == t.simple_spec[:num_colonies] && t.simple_spec[:plate_ids].length == t.simple_spec[:primer_ids]
         sample_check = true
-        t.simple_spec[:plated_ids].each_with_index do |pid,idx|
+        t.simple_spec[:plate_ids].each_with_index do |pid,idx|
           sample_check = sample_check && find(:item, id: id)[0].object_type.name == "E coli Plate of Plasmid"
           t.simple_spec[:primer_ids][idx].each do |prid|
             sample_check = sample_check && find(:sample, id: prid)[0].sample_type.name == "Primer" && find(:sample, id: prid)[0].in("Primer Aliquot")[0]
