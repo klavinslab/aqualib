11d9d9e60683d8ad5d587d37b9235760a1c3727c
sunshineyyy <yaoyu.yang88@gmail.com>
2015-02-23 17:27:57 -0800

    develop new protocol

---
+++ workflows/general/relocate_items.rb
@@ -0,0 +1,42 @@
+needs "aqualib/lib/standard"
+needs "aqualib/lib/cloning"
+
+
+
+class Protocol
+
+  include Standard
+  include Cloning
+
+  def arguments
+    {
+      io_hash: {},
+      "ids Yeast Plate"=> [22673,22674,22675],
+      debug_mode: "Yes"
+    }
+  end
+
+  def main
+    io_hash = input[:io_hash]
+    io_hash = input if !input[:io_hash] || input[:io_hash].empty?
+    io_hash[:debug_mode] = input[:debug_mode] || "No"
+
+    if io_hash[:debug_mode].downcase == "yes"
+      def debug
+        true
+      end
+    end
+    items = io_hash[:ids].collect {|id| find(:item, id:id)[0]}
+    take items, interactive: true
+
+    items.each do |i|
+      i.store
+      i.reload
+    end
+
+    release items, interactive: true, method: "boxes"
+
+
+
+  end # main
+end # Protocol
