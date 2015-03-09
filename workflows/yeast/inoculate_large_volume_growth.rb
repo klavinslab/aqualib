5bcb952a6fcf393a5ffcf9cc1347285dfd162044
sunshineyyy <yaoyu.yang88@gmail.com>
2015-03-08 18:36:50 -0700

    create new file with the aim of refactor 015_inoculate_large_volume_growth.rb

--- workflows/yeast/inoculate_large_volume_growth.rb
+++ workflows/yeast/inoculate_large_volume_growth.rb
@@ -14,9 +14,9 @@ class Protocol
     {
       io_hash: {},
       #Enter the overnight ids that you are going to start overnight with
-      "yeast_overnight_ids Yeast Overnight Suspension" => [8437,8431,8426],
-      #Enter the media type you are going to use
+      "yeast_overnight_ids Yeast Overnight Suspension" => [25980,13576,13577],
       large_volume: 50,
+      #Enter the media type you are going to use
       media_type: "800 mL YPAD liquid (sterile)",
       debug_mode: "Yes"
     }
@@ -25,21 +25,22 @@ class Protocol
   def main
     io_hash = input[:io_hash]
     io_hash = input if !input[:io_hash] || input[:io_hash].empty?
+    io_hash = { debug_mode: "No", large_volume: 50, yeast_culture_ids: [], media_type: "800 mL YPAD liquid (sterile)" }.merge io_hash
     if io_hash[:debug_mode].downcase == "yes"
       def debug
         true
       end
     end
-    large_volume = io_hash[:large_volume] || 50
-  	yeast_overnights = io_hash[:yeast_overnight_ids].collect{|yid| find(:item, id: yid )[0]}
+
+  	yeast_overnights = io_hash[:yeast_overnight_ids].collect{ |yid| find(:item, id: yid )[0] }
   	yeast_cultures = []
   	yeast_overnights.each do |y|
   		yeast_culture = produce new_sample y.sample.name, of: "Yeast Strain", as: "Yeast 50ml culture"
-      yeast_culture.location = "B13.125"
+      yeast_culture.location = "30 C shaker incubator"
       yeast_culture.save
   		yeast_cultures.push yeast_culture
   	end
-    io_hash = ({ yeast_culture_ids: [] }).merge io_hash
+
     if yeast_overnights.length == 0
       show {
         title "No inoculation required"
@@ -53,22 +54,23 @@ class Protocol
 
     show {
       title "Media preparation"
-      check "Grab #{yeast_overnights.length} of 250 mL Baffled Flask"
-      check "Add #{large_volume} mL of #{media_type} into each 250 mL Baffled Flask"
-      check "Label each flask with a piece of tape using following ids"
-      note (yeast_cultures.collect {|x| x.id})
       warning "Work in the media bay for media prepartion"
+      check "Grab #{yeast_overnights.length} of 250 mL Baffled Flask."
+      check "Add #{io_hash[:large_volume]} mL of #{media_type} into each 250 mL Baffled Flask."
+      check "Label each flask with a piece of tape using following ids:"
+      note (yeast_cultures.collect {|x| x.id})
     }
 
   	take yeast_overnights, interactive: true
     
-    tab = [["Flask ids","Yeast Overnight ids","Volume (µL)"]]
+    innoculation_tab = [["Flask ids","Yeast Overnight ids","Volume (µL)"]]
     yeast_overnights.each_with_index do |y,idx|
-      tab.push(["#{yeast_cultures[idx].id}",{ content: "#{y.id}", check: true },{ content: "#{large_volume/50} mL" }])
+      innoculation_tab.push(["#{yeast_cultures[idx].id}",{ content: "#{y.id}", check: true },{ content: "#{io_hash[:large_volume] / 50} mL" }])
     end
+
     show {
       title "Inoculate yeast overnights into flasks"
-      table tab
+      table innoculation_tab
     }
     if io_hash[:task_ids]
       io_hash[:task_ids].each do |tid|
@@ -77,16 +79,17 @@ class Protocol
       end
     end
 
-    delete yeast_overnights
+    #delete yeast_overnights
 
     show {
       title "Discard yeast overnights"
-      note "Discard yeast overnights with the following ids. If it is plastic tube, push down the cap and discard into biohazard box. If it is glass tube, place in the dish washing station."
+      note "Discard yeast overnights with the following ids. If it is a plastic tube, push down the cap to seal the tube and discard into biohazard box. If it is a glass tube, place it in the dish washing station."
       note yeast_overnights.collect { |y| "#{y}"}
     }
 
     release yeast_overnights
     release yeast_cultures, interactive: true, method: "boxes"
+
     io_hash[:yeast_culture_ids] = yeast_cultures.collect {|x| x.id}
 
     return { io_hash: io_hash }
