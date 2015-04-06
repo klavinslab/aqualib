b7b778af9a8cf2d14d8f39bc83b48eef29be01d4
sunshineyyy <yaoyu.yang88@gmail.com>
2015-04-05 21:56:11 -0700

    add return values true or errors array

--- workflows/validator/yeast_strain_QC.rb
+++ workflows/validator/yeast_strain_QC.rb
@@ -2,8 +2,9 @@ class Validator
 
   def check task
     t = task
+    errors = [] # an array to store errors
     length_check = t.simple_spec[:yeast_plate_ids].length == t.simple_spec[:num_colonies].length
-    t.notify "yeast_plate_ids need to have the same array length with num_colonies.", job_id: jid if !length_check
+    errors.push "yeast_plate_ids need to have the same array length with num_colonies." if !length_check
     t[:yeast_plate_ids] = { ready_to_QC: [], not_ready_to_QC: [] }
     t.simple_spec[:yeast_plate_ids].each_with_index do |yid, idx|
       primer1 = find(:item, id: yid)[0].sample.properties["QC Primer1"].in("Primer Aliquot")[0]
@@ -12,19 +13,22 @@ class Validator
         t[:yeast_plate_ids][:ready_to_QC].push yid
       else
         t[:yeast_plate_ids][:not_ready_to_QC].push yid
-        t.notify "QC Primer 1 for yeast plate #{yid} does not have any primer aliquot.", job_id: jid if !primer1
-        t.notify "QC Primer 2 for yeast plate #{yid} does not have any primer aliquot.", job_id: jid if !primer2
-        t.notify "num_colonies for yeast plate #{yid} need to be a number between 0,10", job_id: jid if !(t.simple_spec[:num_colonies][idx] || 0).between?(0, 10)
+        errors.push "QC Primer 1 for yeast plate #{yid} does not have any primer aliquot." if !primer1
+        errors.push "QC Primer 2 for yeast plate #{yid} does not have any primer aliquot." if !primer2
+        errors.push "num_colonies for yeast plate #{yid} need to be a number between 0,10" if !(t.simple_spec[:num_colonies][idx] || 0).between?(0, 10)
       end
     end
+
     ready_conditions = length_check && t[:yeast_plate_ids][:ready_to_QC].length == t.simple_spec[:yeast_plate_ids].length
 
     if ready_conditions
       set_task_status(t, "ready") if t.status != "ready"
       t.save
+      return true
     else
       set_task_status(t, "waiting") if t.status != "waiting"
       t.save
+      return errors
     end
 
   end
