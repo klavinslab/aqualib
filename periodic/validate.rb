33ae660c865f993218231656005da5320b287215
Eric Klavins <klavins@uw.edu>
2015-04-05 20:33:31 -0700

    checking daily tasks

--- periodic/validate.rb
+++ periodic/validate.rb
@@ -3,7 +3,7 @@ class Validator
   def check task
 
     # g = find(:group,name: task.spec[:group])
-    return [ "Group: #{task.spec[:group]}" ]
+    return [ "Group: #{task.spec.inspect}" ]
 
   end
 
