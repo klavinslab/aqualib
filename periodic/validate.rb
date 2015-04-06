6dd37deb53350356ecce8a075cf4027f3981870c
Eric Klavins <klavins@uw.edu>
2015-04-05 20:37:07 -0700

    checking daily tasks

--- periodic/validate.rb
+++ periodic/validate.rb
@@ -3,7 +3,9 @@ class Validator
   def check task
 
     g = Group.find_by_name(task.spec[:group])
-    return [ "Group: '#{task.spec[:group]}', length: #{g.length}" ]
+    return [ "Could not find group '#{task.spec[:group]}'"] unless g
+
+    true
 
   end
 
