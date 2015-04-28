c23eb1d7eee652138612c1c2ae7ab5ab50aa1f87
jvrana <jvrana@uw.edu>
2015-04-27 23:48:12 -0700

    Create test.rb

---
+++ sandbox/Justin/test.rb
@@ -0,0 +1,21 @@
+needs "aqualib/lib/standard"
+needs "aqualib/lib/cloning"
+
+class Protocol
+  include Stanard
+  include Cloning
+  
+  def arguments
+    {
+      samples_ids: [5913, 5914]
+    }
+  end
+  
+  def main
+    show
+      {
+        title "This is a test protocol"
+        note "this is some note"
+      }
+  end
+end
