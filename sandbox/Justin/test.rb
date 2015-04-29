d70888003c382e2ce94e8e71eb62a181c2761feb
jvrana <jvrana@uw.edu>
2015-04-28 18:03:33 -0700

    Update test.rb

--- sandbox/Justin/test.rb
+++ sandbox/Justin/test.rb
@@ -18,7 +18,7 @@ class Protocol
     io_hash = input[:io_hash]
     io_hash = input if !input[:io_hash] || input[:io_hash].empty?
     io_hash = {yeast_ids: [2701, 2697], debug_mode: "No", item_choice_mode: "No"}.merge io_hash
-    ygc = io_hash[:yeast_ids].collect {|y| find(:sample,{id: yid})[0].in("Yeast Glycerol Stock")[0]}
+    ygc = io_hash[:yeast_ids].collect {|y| find(:sample,{id: y})[0].in("Yeast Glycerol Stock")[0]}
     take ygc, interactive: true,  method: "boxes"
   end
   
