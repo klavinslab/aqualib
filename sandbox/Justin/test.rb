c2c54b866ee33aa164d38636b60823d36f914c69
jvrana <jvrana@uw.edu>
2015-06-24 09:15:55 -0700

    Update test.rb

--- sandbox/Justin/test.rb
+++ sandbox/Justin/test.rb
@@ -17,9 +17,8 @@ class Protocol
   def main
     io_hash = input[:io_hash]
     io_hash = input if !input[:io_hash] || input[:io_hash].empty?
-    io_hash = {yeast_ids: [2701, 2697], debug_mode: "No", item_choice_mode: "No"}.merge io_hash
-    ygc = io_hash[:yeast_ids].collect {|y| choose_sample find(:sample,{id: y})[0].name, object_type: "Yeast Plate"}
-    tab = [["Glycerol Stock id", "Loction"]]
+    io_hash = {sample_ids: [2701, 2697], debug_mode: "No", item_choice_mode: "No"}.merge io_hash
+    ygc = io_hash[:sample_ids].collect {|y| choose_sample find(:sample,{id: y})[0].name, object_type: "Plasmid Stock"}
     take ygc, interactive: true, method: "boxes"
   end
   
