4598c611bdeab88285b4ba48a328c3c2697bf765
jvrana <jvrana@uw.edu>
2015-07-09 14:50:00 -0700

    Update test.rb

--- sandbox/Justin/test.rb
+++ sandbox/Justin/test.rb
@@ -9,16 +9,16 @@ class Protocol
   def arguments
     {
       io_hash: {},
-      plasmid_ids: [7869],
-      debug_mode: "No",
+      sample_ids: [6236, 6235],
+      debug_mode: "Yes",
     }
   end
   
   def main
     io_hash = input[:io_hash]
     io_hash = input if !input[:io_hash] || input[:io_hash].empty?
-    io_hash = {sample_ids: [2701, 2697], debug_mode: "No", item_choice_mode: "No"}.merge io_hash
-    ps = io_hash[:sample_ids].collect {|y| choose_sample find(:sample,{id: y})[0].name, object_type: "Plasmid Stock"}
+    ygc = io_hash[:sample_ids].collect {|y| choose_sample find(:sample,{id: y})[0].name, object_type: "Plasmid Stock"}
+    take ygc, interactive: true, method: "boxes"
   end
   
 end
