d394e87d9d513dcfb5b67e5838d5e7fe936f33d9
Eric Klavins <klavins@uw.edu>
2015-07-26 09:40:00 -0700

    test workflow protocol

--- auto/test.rb
+++ auto/test.rb
@@ -7,7 +7,7 @@ class Protocol
 
   def main
 
-    # o = Op.new input
+    o = op input
 
     # take o.inputs(:fwd).first_items, method: "boxes"
     # take o.inputs(:rev).first_items, method: "boxes"  
