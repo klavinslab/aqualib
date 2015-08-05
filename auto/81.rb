2ae38144912a000eb13d22d4896f72d6de39b9f0
Eric Klavins <klavins@uw.edu>
2015-08-05 12:57:42 -0700

    syntax

--- auto/81.rb
+++ auto/81.rb
@@ -16,9 +16,9 @@ class Protocol
     stripwells.slots do |index,slot|
       puts o.output.fragment.get
       slot.sample                   = o.output.fragment.samples[index]
-      slot.ingredients[:fwd]        = { id: i.input.fwd.item.id, volume: 1 }
-      slot.ingredients[:rev]        = { id: i.input.rev.item.id, volume: 2 }
-      slot.ingredients[:template]   = { id: i.input.template.item.id, volume: 3 }
+      slot.ingredients[:fwd]        = { id: o.input.fwd.item.id, volume: 1 }
+      slot.ingredients[:rev]        = { id: o.input.rev.item.id, volume: 2 }
+      slot.ingredients[:template]   = { id: o.input.template.item.id, volume: 3 }
       slot.ingredients[:master_mix] = { volume: 4 }
       slot.ingredients[:water]      = { volume: 5 }
     end
