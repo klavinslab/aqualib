57bf22c699ff019051508ed23a34f48f1e573373
Eric Klavins <klavins@uw.edu>
2015-08-13 15:12:03 -0700

    after save method for daily and weekly tasks

--- periodic/after_save.rb
+++ periodic/after_save.rb
@@ -1,11 +1,7 @@
-class Task
+def after_save
+  self.notify "Hello from after_save.rb: #{f 123}"
+end
 
-  def after_save
-    self.notify "Hello from after_save.rb: #{f 123}"
-  end
-
-  def f x
-    x+1
-  end
-
+def f x
+  x+1
 end
