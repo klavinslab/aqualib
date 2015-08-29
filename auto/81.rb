aa80ea229e179a1c48e9eb75dcc455efd6f41948
Eric Klavins <klavins@uw.edu>
2015-08-28 17:06:11 -0700

    working on workflow process updater

--- auto/81.rb
+++ auto/81.rb
@@ -1,19 +1,3 @@
-
-class Timer
-
-  def initialize
-    @t = Time.now
-    @i = 1
-  end
-
-  def click
-    puts "#{@i}: #{((Time.now-@t).seconds*1000).to_i} ms"
-    @t = Time.now
-    @i += 1
-  end
-
-end
-
 class Protocol
 
   def debug
@@ -22,13 +6,11 @@ class Protocol
 
   def main
 
-    t = Timer.new
     o = op input
-    t.click
     o.input.all.take
-    t.click
     stripwells = o.output.fragment.new_collections
-    t.click
+    
+    # collection wise
     stripwells.slots do |index,slot|
       if index < o.output.fragment.samples.length 
         o.output.fragment.associate index, slot
@@ -39,9 +21,20 @@ class Protocol
         slot.ingredients[:water]      = { volume: 5 }
       end
     end
-    t.click
+
+    # threadwise
+    # (o.threads.reject { |t| t[:error] }).each do |thread|
+    #   stripwells.slot[thread.index] = thread[:fragment][:sample]
+    #   stripwells.slot[thread.index].ingredients[:rev] = { id: thread[:rev][:item], volume: 1 }
+    # end
+
+    # bothwise
+    # o.threads.zip(stripwells, include_errors: false).each do |thread, slot| 
+    #   slot.ingredients[:rev] = { id: thread[:rev][:item], volume: 1} 
+    # end
+    
     o.output.fragment.produce
-    t.click
+    
     stripwells.length.times do |i|
       show {
         title "Load primers and template for stripwell #{stripwells[i].id}"
@@ -52,21 +45,20 @@ class Protocol
         table stripwells.table(i, id: "Stripwell", col: "Well", master_mix: "Master Mix", water: "Water")
       }
     end
-    t.click
+    
     data = show {
       title "Put stripwells in thermocycler"
       note "Set the annealing temperature to #{o.parameter.annealing_temperature[0]}"
       get "number", var: "tc", label: "What thermocycler was used?", default: 1
     }
-    t.click
+    
     o.data.tc.get.each do |d|
+      puts "setting d = #{d} to data[:tc] = #{data[:tc]}"
       d = data[:tc]
     end
-    t.click
+    
     o.input.all.release
-    t.click
     o.output.all.release
-    t.click
     return o.result     
 
   end
