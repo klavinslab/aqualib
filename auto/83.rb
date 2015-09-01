83c6ebfaa6e5fa8901c95a62ed3f06471087b673
Eric Klavins <klavins@uw.edu>
2015-09-01 16:00:19 -0700

    working on run gel

--- auto/83.rb
+++ auto/83.rb
@@ -11,7 +11,7 @@ class Protocol
     o.input.all.take
 
     # load the gel with ladder
-    o.input.gel.collections.each |gel| do 
+    o.input.gel.collections.each do |gel| 
       gel.set 0, 0, o.input.ladder.instances.first[:item]
     end
 
