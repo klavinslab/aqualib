42a8cf5200bf3739a4c72d311474e4d482eeb712
Eric Klavins <klavins@uw.edu>
2015-09-03 16:22:00 -0700

    threading

--- auto/84.rb
+++ auto/84.rb
@@ -8,9 +8,15 @@ class Protocol
 
     o = op input
 
+    o.input.all.take
+
     show do 
-      title "Generic Protocol"
+      title "Cut gel the"
     end
+
+    o.output.all.produce
+    o.input.all.release
+    o.output.all.release
 
     return o.result
 
