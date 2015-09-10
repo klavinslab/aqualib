6fa7466348f7e390a48fabecfcaa583c47ddeb2e
Eric Klavins <klavins@uw.edu>
2015-09-10 09:51:28 -0700

    Created generic protocol for operation 179

---
+++ auto/179.rb
@@ -0,0 +1,25 @@
+class Protocol
+
+  def debug
+    true
+  end
+
+  def main
+
+    o = op input
+
+    o.input.all.take
+    o.output.all.produce
+
+    show do
+      title "Instructions here"
+    end
+
+    o.input.all.release
+    o.output.all.release
+
+    return o.result
+
+  end
+
+end
