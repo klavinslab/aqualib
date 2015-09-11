4cb5ccc0793214e5b249b3bbab5ec0211e54b946
Eric Klavins <klavins@uw.edu>
2015-09-11 10:21:51 -0700

    Created generic protocol for operation 5

--- auto/5.rb
+++ auto/5.rb
@@ -7,21 +7,8 @@ class Protocol
     o.input.all.take
     o.output.all.produce
 
-    ingredients = Table.new(
-      slice: "Gel slice id",
-      stock: "Fragment stock id"
-    )
-
-    o.threads.each do |thread|
-      ingredients
-        .slice(thread.input.fragment.item_id)
-        .stock(thread.output.fragment.item_id)
-        .append
-    end
-
     show do
-      title "Purify the gel slices and put the results in the 1.5 uL tubes"
-      table ingredients.all.render
+      title "Instructions here"
     end
 
     o.input.all.release
