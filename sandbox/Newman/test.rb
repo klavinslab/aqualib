614b7507603b123d1935b96d6929f372bf14b26a
TheNumbhead <garrett.s.newman@gmail.com>
2015-09-14 11:50:33 -0700

    Create test.rb
    optional extended description

---
+++ sandbox/Newman/test.rb
@@ -0,0 +1,22 @@
+class Protocol
+
+  def main
+  
+    show do
+      title "Hello Bootcamp!"
+      note "Seriously? 9-1 for real???"
+      table [["A", "2"],[3,4]]
+      check "Please check this. (please!)"
+      select ["C","D"], var: "choice", label: "Do the thing", default: 1
+    end
+    
+    x = result[:choice]
+    
+    show {
+      title "Here There is a Title. Also, you entered #{x}."
+    }
+    
+    return { x: x, y: "that was fun" }
+    
+  end
+end
