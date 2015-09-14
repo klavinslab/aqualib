c24fd618d9d2bcd88911f038c7c486a53ac49a81
Aditya Gandhi <agandhi@uw.edu>
2015-09-14 11:49:08 -0700

    Create test.rb

---
+++ sandbox/Aditya/test.rb
@@ -0,0 +1,18 @@
+Class Protocol
+
+def main
+
+show{
+title: "Bootcamp"
+note "Test Text"
+check: "Check This"
+}
+x==result[:x]
+
+show do
+  title "You entered #x"
+end
+
+return {x:x,y:"That was fun"}
+  end 
+end
