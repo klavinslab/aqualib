2b252d29b36ebf0e6b42abf7aa1562a99c4c6ee5
Yaoyu Yang <yaoyu.yang88@gmail.com>
2015-09-14 11:47:27 -0700

    Update test.rb

--- sandbox/yang/test.rb
+++ sandbox/yang/test.rb
@@ -2,9 +2,17 @@ class Protocol
 
   def main
   
-    show {
-      
+    result = show {
+      title "Test"
+      note "dage"
+      table [["A","B"],[1,2]]
+      check "Check"
+      select [ "A", "B", "C" ], var: "choice", label: "Choose something", default: 1
     }
+    
+    x = result[:choice]
+    
+    return { x: x, y: "that was fun" }
   
   end
   
