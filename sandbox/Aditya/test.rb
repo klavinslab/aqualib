5beb7fecd5e008d213679cf863812561cbe3ad8a
Aditya Gandhi <agandhi@uw.edu>
2015-09-14 11:57:41 -0700

    Update test.rb

--- sandbox/Aditya/test.rb
+++ sandbox/Aditya/test.rb
@@ -2,17 +2,17 @@ Class Protocol
 
 def main
 
-show{
+resu;lt=show{
 title: "Bootcamp"
 note "Test Text"
 check: "Check This"
 }
-x==result[:x]
+x=result[:x]
 
 show do
   title "You entered #x"
 end
 
-return {x:x,y:"That was fun"}
+return {x:result[:x],y:"That was fun"}
   end 
 end
