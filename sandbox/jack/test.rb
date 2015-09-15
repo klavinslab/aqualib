8a577ac0ffba3da09efdcf1e4e574c43d48bc828
joonseokii <joonseokii@yahoo.com>
2015-09-15 09:11:26 -0700

    Update test.rb

--- sandbox/jack/test.rb
+++ sandbox/jack/test.rb
@@ -1,20 +1,21 @@
 class Protocol
 
   def main
-  
+ 
     result = show {
-      title "This is cool!"
-      note "What am I doing"
-      table[["A","C"], [1,3]]
-      check "Check this!"
-      select [ "A", "B", "C" ], var: "x", label: "Choose something", default: 1
+      title "Hello World!"
+      note "What is this."
+      table [["A","B"],[1,2]]
+      check "Check this"
+      select [ "A", "B", "C" ], var: "x", label: "Choose one", default: 1
     }
     
     show do
-     title "You entered #{result[:x]}"
+      title "You entered #{result[:x]}"
     end
     
-    return {x: result[:x], y:"That was fun"}
+    return { x: result[:x], y: "that was fun" }
+  
   end
   
 end
