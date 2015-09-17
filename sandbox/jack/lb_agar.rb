67e4fce6d0fb07f90af87aed8361fc7a21ecb39c
joonseokii <joonseokii@yahoo.com>
2015-09-17 00:30:44 -0700

    Create lb_agar.rb
    test protocol

---
+++ sandbox/jack/lb_agar.rb
@@ -0,0 +1,41 @@
+class Protocol
+
+  def main
+  
+    show {
+      title "LB Agar"
+      note "Description: This protocol is for the preperation of LB Agar, the common plate media for growing bacteria cells.
+      note "Total duration: 3 hours"
+      note "Total worktime: 30 minutes"
+    }
+    
+    show {
+      title "Get Bottle and Stir Bar: Retrieve one Glass Liter Bottle from the glassware rack and one Medium Magnetic Stir Bar 
+          from the dishwashing station, bring to weigh station. Put the stir bar in the bottle."
+    }
+    
+    show {
+      title "Weigh Agar: Using the gram scale, large weigh boat, and chemical spatula, weigh out 29.6 grams of LB Agar
+          powder and pour into the bottle."
+      }
+      
+    show {
+      title "Using the graduated cylinder measure out 800 mL of DI water."
+    }
+    
+    show {
+      title "Measure out Water: fill bottle with DI water, shake to get all powder solvated."
+    }
+    
+    show {
+      title "Autoclave: stick autoclave tape on top of bottle cap, loosen top and autoclave at 110C for 15 minutes."
+    }
+    
+    show {
+      title "Remove from Autoclave: put on thermal gloves and take bottle out of autoclave, place on stir plate."
+    }
+    
+    show {
+      title "Stir: Heat to 65C while stirring at 700 rpm."
+    }
+      
