42a473686d8930eeb32476dec21bf2caca177f6c
TheNumbhead <garrett.s.newman@gmail.com>
2015-09-16 19:23:12 -0700

    Update e_coli_competent.rb

--- sandbox/Newman/e_coli_competent.rb
+++ sandbox/Newman/e_coli_competent.rb
@@ -37,11 +37,47 @@ class Protocol
 		show {
 			title "Dilute culture"
 			note "Dilute overnight culture for 1 minute and 50 seconds into #{3 * aliquot_num} mL fresh broth (#{60 * aliquot_num} uL overnight culture)."
+			timer initial: { hours: 0, minutes: 1, seconds: 50}
 		}
 
 		# Step 5
 		show {
-			title "Das it!"
+			title "Incubate and check OD600"
+			note "Incubate at 37 C for 1-3 hours. Check OD600 on Nanodrop after 1 hour. The target OD600 is 0.4-0.6."
+			note "Note: Multiply the absorbance value at 600 nm measured by the Nanodrop by a factor of 10 to get OD600."
+			timer initial: { hours: 1, minutes: 0, seconds: 0}
+		}
+
+		# Step 6
+		show {
+			title "Add Water"
+			note "Place water from freezer in ice bath, and add 5-10 mL room temperature molecular grade water and shake to cool."
+		}
+
+		2.times {
+			# Step 7
+			show {
+				title "Run in Centrifuge"
+				note "Pellet cell culture in 1.5 mL tubes (1 mL culture per tube) by running in refrigerated centrifuge (4 C) for 1 minute at 6000 xg."
+				timer initial: { hours: 0, minutes: 1, seconds: 0}
+			}
+
+			# Step 8
+			show {
+				title "Resuspend Cells"
+				note "Remove supernatant. Add 1 mL ice cold water and resuspend cells."
+			}
+		}
+
+		# Step 9
+		show {
+			title "Resuspend Cells"
+			note "Resuspend cells in 40 uL ice cold water."
+		}
+
+		# Step 10
+		show {
+			title "Follow electroporation protocol."
 		}
 	end
 end
