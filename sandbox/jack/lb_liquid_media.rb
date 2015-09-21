f084a7d9d2bc59667b5efecdd3a291e0732c5bc8
joonseokii <joonseokii@yahoo.com>
2015-09-21 12:31:38 -0700

    Update lb_liquid_media.rb

--- sandbox/jack/lb_liquid_media.rb
+++ sandbox/jack/lb_liquid_media.rb
@@ -22,16 +22,6 @@ class Protocol
       title "Close liter bottle and shake until all contents are solvated"
     }
     
-    show {
-      title "Stick autoclave tape on top of the bottle"
-    }
-    
-    show {
-      title "Loosen cap and autoclave at 110C for 15 minutes"
-      timer initial: { hours: 0, minutes: 15, seconds: 0}
-    }
-    
-
     o.input.all.release
     o.output.all.release
 
