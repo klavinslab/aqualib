17891b879873e686adf0d85d3cc95c4b3f505a96
joonseokii <joonseokii@yahoo.com>
2015-09-21 13:01:23 -0700

    Update lb_liquid_media.rb

--- sandbox/jack/lb_liquid_media.rb
+++ sandbox/jack/lb_liquid_media.rb
@@ -5,6 +5,14 @@ class Protocol
 
     o.input.all.take
     o.output.all.produce
+    
+    show {
+      title "LB Liquid Media"
+      note "Description: This prepares a bottle of LB Media for growing bacteria"
+      note "Total duration: 3 hours"
+      note "Total worktime: 30 minutes"
+      warning "To add antibotics, wait for contents to cool to 40 C and add the appropriate amount of antibiotics"
+    }
 
     show {
       title "Place large weigh boat on gram scale and zero"
