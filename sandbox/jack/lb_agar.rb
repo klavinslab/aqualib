c842331c947c3481397fdbe58b310d38d2b63c21
joonseokii <joonseokii@yahoo.com>
2015-09-30 11:18:46 -0700

    Update lb_agar.rb

--- sandbox/jack/lb_agar.rb
+++ sandbox/jack/lb_agar.rb
@@ -13,19 +13,23 @@ class Protocol
     }
     
     show {
-      note "Get Bottle and Stir Bar: Retrieve one Glass Liter Bottle from the glassware rack and one Medium Magnetic Stir Bar from the dishwashing station, bring to weigh station. Put the stir bar in the bottle."
+      title "Get Bottle and Stir Bar"
+      note "Retrieve one Glass Liter Bottle from the glassware rack and one Medium Magnetic Stir Bar from the dishwashing station, bring to weigh station. Put the stir bar in the bottle."
     }
     
     show {
-      note "Weigh Agar: Using the gram scale, large weigh boat, and chemical spatula, weigh out 29.6 grams of LB Agar powder and pour into the bottle."
+      title "Weigh Agar"
+      note "Using the gram scale, large weigh boat, and chemical spatula, weigh out 29.6 grams of LB Agar powder and pour into the bottle."
       }
       
     show {
+      title "Measure Water"
       note "Using the graduated cylinder measure out 800 mL of DI water."
     }
     
     show {
-      note "Measure out Water: fill bottle with DI water, shake to get all powder solvated."
+      title "Mix solution"
+      note "fill bottle with DI water, shake to get all powder solvated."
     }
     
     o.input.all.release
