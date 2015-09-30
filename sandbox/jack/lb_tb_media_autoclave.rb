fbd79083d1dec86db10202377c2d91ffb189842d
joonseokii <joonseokii@yahoo.com>
2015-09-30 09:35:49 -0700

    Update lb_tb_media_autoclave.rb

--- sandbox/jack/lb_tb_media_autoclave.rb
+++ sandbox/jack/lb_tb_media_autoclave.rb
@@ -8,12 +8,26 @@ class Protocol
     o.output.all.produce
 
     show {
-      title "Stick autoclave tape on top of the bottle"
+      title "Autoclave for Bacteria"
+      note "Description: This protocol is for sterilizing the media used for bacteria"
+      warning "Wait until the agar has cooled enough to touch with bare hands and add the appropriate amount of antibiotic while stirring"
     }
     
     show {
-      title "Loosen cap and autoclave at 110C for 15 minutes"
+      note "Stick autoclave tape on top of the bottle"
+    }
+    
+    show {
+      note "Loosen cap and autoclave at 110C for 15 minutes"
+      note "Click next to start timer"
+    }
+    
+    show {
       timer initial: { hours: 0, minutes: 15, seconds: 0}
+    }
+    
+    show {
+      warning "Stuff caked at the bottom: after autoclaving, if there is stuff caked at the bottom, do not use this batch, remake the media and make sure that everything is solvated before autoclaving (shake harder)"
     }
 
     o.input.all.release
