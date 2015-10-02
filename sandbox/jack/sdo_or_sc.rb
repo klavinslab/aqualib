8e31b56ae33271c755a05609179d7e0764ca1289
joonseokii <joonseokii@yahoo.com>
2015-10-02 11:04:24 -0700

    Create sdo_or_sc.rb

---
+++ sandbox/jack/sdo_or_sc.rb
@@ -0,0 +1,16 @@
+class Protocol
+  
+  def main
+    o = op input
+    
+    o.input.all.take
+    o.output.all.produce
+    
+    show {
+      title "Make SDO or SC Media"
+      note "Description: Makes 800 mL of synthetic dropout (SDO) or synthetic complete (SC) media with 2% glucose and adenine supplement (800mL)"
+    }
+    
+    show {
+      
+    }
