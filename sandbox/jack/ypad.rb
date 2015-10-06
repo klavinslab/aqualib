7eda989b3f30dcaa90c05f44067d7a703bd469ee
joonseokii <joonseokii@yahoo.com>
2015-10-05 23:12:48 -0700

    Update ypad.rb

--- sandbox/jack/ypad.rb
+++ sandbox/jack/ypad.rb
@@ -6,39 +6,32 @@ class Protocol
     o.input.all.take
     o.output.all.produce
     
+    boo = o.output.all.item_ids
+    
     show {
       title "Make YPAD Media"
       note "Description: Make 800 mL of yeast extract-peptone-dextrose medium + adenine (YPAD)"
     }
     
     show {
-      note "Label the empty 1000 mL bottle"
-    }
-    
-    show {
+      title "Weigh Chemicals"
       note "Weight out yeast extract, peptone, dextrose, and adenine sulfate (and optional agar) and add to 1000 mL bottle"
     }
     
     show {
+      title "Add diH2O"
       note "Add 500 mL dIH2O to bottle, close cap tightly and shake to mix"
     }
     
     show {
+      title "Add Water and Mix"
       note "Add water to 800 mL mark on bottle, shake again to mix"
     }
     
     show {
-      note "Place cap on bottle loosely"
+      title "Label Bottle"
+      note "Label the bottle with 'YPAD', 'Your initials', '#{ boo[0] }'"
     }
-    
-    show {
-      note "Stick autoclave tape on the cap"
-    }
-    
-    show {
-      note "Autoclave 15 minutes at 110°C, minimal drying cycle (optional cycle)."
-    }
-    
     
     o.input.all.release
     o.output.all.release
