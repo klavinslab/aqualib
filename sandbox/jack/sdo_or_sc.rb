fe7f9ad9e3b2ac96e12a33ad916c13182dd48f21
joonseokii <joonseokii@yahoo.com>
2015-11-25 11:21:09 -0800

    Update sdo_or_sc.rb

--- sandbox/jack/sdo_or_sc.rb
+++ sandbox/jack/sdo_or_sc.rb
@@ -49,6 +49,7 @@ class Protocol
     show {
       title "#{label}"
       note "Description: Makes 800 mL of #{label} media with 2% glucose and adenine supplement"
+      note "#{includeAcids}"
     }
     
     show {
@@ -84,7 +85,7 @@ class Protocol
     
     show {
       title "Label Bottle"
-      note "Label the bottle with '#{label.gsub(/(unsterile)/, '')}', 'Date', 'Your initials', '#{item_id[0]}'"
+      note "Label the bottle with '#{label.gsub(/\(unsterile\)/, '')}', 'Date', 'Your initials', '#{item_id[0]}'"
     }
     
     release ingredients, interactive: true
