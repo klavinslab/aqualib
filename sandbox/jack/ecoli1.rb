6982d7fed5b7dcd1bd6746c40821f940a8329ff5
joonseokii <joonseokii@yahoo.com>
2015-12-30 01:44:24 -0800

    Update ecoli1.rb

--- sandbox/jack/ecoli1.rb
+++ sandbox/jack/ecoli1.rb
@@ -12,10 +12,10 @@ class Protocol
 		take [bottle_1L, bottle_500mL, broth, glycerol], interactive: true
 
 		lb_liquid = produce new_object "800 mL LB liquid (unsterile)"
-		#glycerol = produce new_object "500 mL 10 Percent Glycerol (unsterile)"
-		#water = produce new_object "1000 mL DI Water (unsterile)"
-		#lb_liquid_sterile = produce new_object "800 mL LB liquid (sterile)"
-		#lb_liquid.mark_as_deleted
+		glycerol = produce new_object "500 mL 10 Percent Glycerol (unsterile)"
+		water = produce new_object "1000 mL DI Water (unsterile)"
+		lb_liquid_sterile = produce new_object "800 mL LB liquid (sterile)"
+		lb_liquid.mark_as_deleted
 
 		show {
 			title "Prepare Bottles"
