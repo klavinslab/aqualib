76d75d3848de6179bc8610512b66e537a46a10d6
joonseokii <joonseokii@yahoo.com>
2016-01-11 02:07:30 -0800

    Update ecoli3.rb

--- sandbox/jack/ecoli3.rb
+++ sandbox/jack/ecoli3.rb
@@ -11,14 +11,14 @@ class Protocol
 		
 		io_hash = input[:io_hash]
 		
-		lb_liquid = find(:item, id: (io_hash[:lb_liquid])[:id])
+		lb_liquid = find(:item, id: (io_hash[:lb_liquid]))
 		take [lb_liquid], interactive: true
 		show {
 			title "Place LB in heat bath"
 			note "Set heat bath to 37"
 			note "Once temperature reaches 37, immerse LB in beads"
 		}
-		io_hash = {new_lb_liquid: lb_liquid}.merge(io_hash)
+		io_hash = {new_lb_liquid: lb_liquid.id}.merge(io_hash)
 		release([lb_liquid], interactive: true)
 		return { io_hash: io_hash }
 	end
