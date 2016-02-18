89dd1ae615cde0ee69250be0d5860509a7202d97
joonseokii <joonseokii@yahoo.com>
2016-02-18 07:51:19 -0800

    Update yeast_media_ypad.rb

--- workflows/media/yeast_media_ypad.rb
+++ workflows/media/yeast_media_ypad.rb
@@ -19,40 +19,62 @@ class Protocol
         set_task_status(task_to_run, "done")
         media = task_to_run.simple_spec[:media_type]
         media_name = find(:sample, id: media)[0].name
+        quantity = task_to_run.simple_spec[:quantity]
         
         if(media_name == "YPAD")
             adenine = find(:item, { object_type: { name: "Adenine (Adenine hemisulfate)" } } )[0] 
             dextrose = find(:item, { object_type: { name: "Dextrose" } } )[0] 
             bacto = find(:item, { object_type: { name: "Bacto Yeast Extract" } } )[0] 
             tryp = find(:item, { object_type: { name: "Bacto Tryptone" } } )[0]
-            produced_media = produce new_sample "YPAD", of: "Media", as: "800 mL Bottle"
-            if(task_to_run.simple_spec[:media_container] == "800 mL Bottle")
-                bottle = find(:item, { object_type: { name: "1 L Bottle" } } )[0]
-            else
-                raise ArgumentError, "Chosen container is not valid"
-            end
+            #produced_media = produce new_sample "YPAD", of: "Media", as: "800 mL Bottle"
         else
             raise ArgumentError, "Chosen media is not valid"
         end
         
-        produced_media.location = "Bench"
-        take [adenine, dextrose, bacto, tryp, bottle], interactive: true
+        bottle = [find(:item, object_type: { name: "1 L Bottle"})[0]] * quantity
         
-        io_hash = {type: "yeast", media: produced_media.id}.merge(io_hash)
+        label = media_name 
+        
+		if(task_to_run.simple_spec[:media_container] == "800 mL Bottle") 
+			multiplier = 1;
+			water = 800
+		elsif(task_to_run.simple_spec[:media_container] == "Agar Plate")
+			multiplier = 1;
+			label += " for Agar"
+			water = 800
+		elsif(task_to_run.simple_spec[:media_container] == "400 mL Bottle")
+			multiplier = 0.5;
+			water = 400
+		elsif(task_to_run.simple_spec[:media_container] == "200 mL Bottle")
+			multiplier = 0.25;
+			water = 200
+		else
+			raise ArgumentError, "Container specified is not valid"
+		end
+        
+		produced_media = Array.new
+		for i in 0..(quantity - 1)
+			produced_media.push(produce new_sample media_name, of: "Media", as: task_to_run.simple_spec[:media_container])
+			produced_media[i].location = "Bench"
+		end
+		
+        take [adenine, dextrose, bacto, tryp] + bottle, interactive: true
+        
+        io_hash = {type: "yeast"}.merge(io_hash)
 
         show {
           title "Make YPAD Media"
-          note "Description: Make 800 mL of yeast extract-tryptone-dextrose medium + adenine (YPAD)"
+          note "Description: Make #{quantity} #{water}mL of yeast extract-tryptone-dextrose medium + adenine (YPAD)"
         }
 
         show {
           title "Weigh Chemicals"
-          note "Weight out 8g yeast extract, 16g tryptone, 16g dextrose, .064g adenine sulfate and add to 1000 mL bottle"
+          note "Weight out #{8 * multiplier}g yeast extract, #{16 * multiplier}g tryptone, #{16 * multiplier}g dextrose, #{0.064 * multiplier}g adenine sulfate and add to 1000 mL bottle(s)"
         }
 
         show {
           title "Measure Water"
-          note "Take the bottle to the DI water carboy and add water up to the 800 mL mark"
+          note "Take the bottle(s) to the DI water carboy and add water up to the #{water} mL mark"
         }
 
         show {
@@ -63,16 +85,16 @@ class Protocol
 
         show {
           title "Cap Bottle"
-          note "Place cap on bottle loosely"
+          note "Place cap(s) on bottle loosely(s)"
         }
 
         show {
-          title "Label Bottle"
-          note "Label the bottle with 'YPAD', 'Date', Your initials'"
+          title "Label Bottle(s)"
+          note "Label the bottle(s) with 'YPAD', 'Date', Your initials'"
         }
 
-        release([bottle])
-        release([adenine, dextrose, bacto, tryp, produced_media], interactive: true)
+        release(bottle)
+        release([adenine, dextrose, bacto, tryp] + produced_media, interactive: true)
 
         return {io_hash: io_hash}
 
