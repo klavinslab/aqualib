a9f1a1378d546aa7edc97326b4336c25153f7650
joonseokii <joonseokii@yahoo.com>
2016-03-01 22:18:11 -0800

    Create move_agar_plates.rb

---
+++ workflows/media/move_agar_plates.rb
@@ -0,0 +1,26 @@
+class Protocol
+
+	def arguments
+	    {
+	    	io_hash: {}
+	    }
+	end
+
+  def main
+	
+	show {
+	
+	}
+	Step 1: Take:
+- Take all plates of a particular media type 
+
+Step 2: Move plates to storage containers
+- Find an empty storage container on top of the media fridge. Remove any labeling tape that might be on the container.
+- Move one of the labels on the plate stack to the front of the storage container.
+- Move plates (first item number - last item number) to the storage container.
+
+Step 3: Return
+- Return plates (first item number - last item number) to the Media Fridge.
+
+  end
+end
