6000945f0c42d6f6b5579b7e2d60c08f6e46039e
joonseokii <joonseokii@yahoo.com>
2016-03-08 01:17:32 -0800

    Update ecoli5.rb

--- sandbox/jack/ecoli5.rb
+++ sandbox/jack/ecoli5.rb
@@ -2,14 +2,14 @@ class Protocol
 	
 	def arguments
     		{
-    			batch: 1,
+    			io_hash: {}
     		}
 	 end
 	
 	def main
 		
 		io_hash = input[:io_hash]
-		io_hash.has_key?(:measurement) ? (tube = find(:item, id: (io_hash[:tube]))[0]) : (tube = find(:item, object_type: {name: "1.5mL tube"})[0])
+		tube = find(:item, object_type: {name: "1.5mL tube"})[0]
 		flask2000 = find(:item, id: (io_hash[:dh5alpha_new]))[0]
 		take [flask2000, tube], interactive: true
 		tube.location = "Bench"
@@ -28,9 +28,12 @@ class Protocol
 			note "Measure OD 600 of aliquot"
 			get "number", var: "measurement", label: "Enter the OD value", default: 0
 		}
-
-		io_hash = {measurement: data[:measurement], tube: tube}.merge(io_hash)
+		if(data[:measurement] <= 0.04) {
+			finished = "no"
+		} else {
+			finished = "yes"
+		}
 		release([tube], interactive: true)
-		return { io_hash: io_hash}
+		return {io_hash: io_hash, done: finished}
 	end
 end
