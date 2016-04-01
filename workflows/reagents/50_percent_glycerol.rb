ed93564551bb3e2b1d16c965f518143ac73f9c73
michelleparks <mnparks@uw.edu>
2016-04-01 15:36:43 -0700

    Create 50_percent_glycerol.rb

---
+++ workflows/reagents/50_percent_glycerol.rb
@@ -0,0 +1,44 @@
+Class Protocol
+
+	def main
+
+		media_type = "50% Glycerol"
+
+		glycerol = find(:item, { object_type: { name: "Glycerol" } } )[0]
+		bottle = find(:item, { object_type: { name: "1 L Bottle" } } )[0]
+
+		show {
+			title "About this protocol"
+			note "This protocol makes #{media_type} for E. coli and yeast media."
+		}
+
+		take [glycerol, bottle], interactive: true
+
+		show {
+			title "Measure out glycerol"
+			check "Find a graduated cylinder."
+			check "Using the graduated cylinder, measure 400 mL Glycerol and pour into the 1 L bottle."
+			warning "Glycerol is very viscous, be sure to add the correct amount to the bottle."
+		}
+
+		show {
+			title "Add DI Water"
+			note "Using the DI water carboy, DI water up to the 800 mL mark on the bottle."
+		}
+
+		show {
+			title "Label"
+			note "Label the bottle with #{media_type} your initials, and the date."
+		}
+
+		media = produce new_sample "50% glycerol", of: "Media", as: "800 mL Liquid"
+
+		media.location = "To be autoclaved area"
+
+
+		release [glycerol, media], interactive: true
+
+		release [bottle]
+
+	end
+end
