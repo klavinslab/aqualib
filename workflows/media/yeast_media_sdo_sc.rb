1c91a1e5b91af8f60ab31975344b63c85191ad73
joonseokii <joonseokii@yahoo.com>
2016-04-08 12:16:57 -0700

    Update yeast_media_sdo_sc.rb

--- workflows/media/yeast_media_sdo_sc.rb
+++ workflows/media/yeast_media_sdo_sc.rb
@@ -62,14 +62,12 @@ class Protocol
 		label += " Agar"
 		water = 400
 		bottle = "500 mL Bottle"
-	        io_hash = {has_agar: "yes"}.merge(io_hash)
 		ingredients += [find(:item,{object_type:{name:"Bacto Agar"}})[0]]
 	elsif(task_to_run.simple_spec[:media_container] == "200 mL Agar")
 		multiplier = 0.25;
 		label += " Agar"
 		water = 200
 		bottle = "250 mL Bottle"
-	        io_hash = {has_agar: "yes"}.merge(io_hash)
 		ingredients += [find(:item,{object_type:{name:"Bacto Agar"}})[0]]
 	else
 		raise ArgumentError, "Container specified is not valid"
