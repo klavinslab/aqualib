fbd45414e0da2e4967d603e58316049da73cb19e
Eric Klavins <klavins@uw.edu>
2016-05-20 19:37:59 -0700

    Updated aqualib/sandbox/klavins/first.rb

--- sandbox/klavins/first.rb
+++ sandbox/klavins/first.rb
@@ -22,6 +22,7 @@ class Protocol
       get "text", var: "y", label: "Enter a string", default: "Hello World"
       get "number", var: "z", label: "Enter a number", default: 555
     }
+    
 
     responses = (x.reject { |k,v| k == :timestamp }).collect { |k,v| "#{k}: #{v}" }
 
