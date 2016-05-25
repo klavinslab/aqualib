6190ef5f04004d51921ae1bd00f8f77ea2c1696b
Yaoyu Yang <yaoyu.yang88@gmail.com>
2016-05-25 14:15:48 -0700

    fix bug

--- utils/change_sample_fileds_plasmid_sequencing_primer.rb
+++ utils/change_sample_fileds_plasmid_sequencing_primer.rb
@@ -14,16 +14,15 @@ class Protocol
   def main
     plasmids = find(:sample, { sample_type: { name: "Plasmid" } })
     changes = []
-    while changes.length < 10
-      plasmids.each do |plasmid|
-        primer_ids_str = plasmid.properties["Sequencing_primer_ids"]
-        primer_ids = primer_ids_str.split(",").map { |s| s.to_i }
-        if primer_ids.all? { |i| i != 0 }
-          primers = primer_ids.collect { |id| find(:sample, id: id)[0] }
-          plasmid.properties["Sequencing Primers"] = primers
-          if plasmid.errors.empty?
-            changes.push "plasmid sequencing primers changed to new format"
-          end
+    plasmids.each do |plasmid|
+      primer_ids_str = plasmid.properties["Sequencing_primer_ids"]
+      primer_ids = primer_ids_str.split(",").map { |s| s.to_i }
+      if primer_ids.all? { |i| i != 0 }
+        primers = primer_ids.collect { |id| find(:sample, id: id)[0] }
+        plasmid.properties["Sequencing Primers"] = primers
+        plasmid.save
+        if plasmid.errors.empty?
+          changes.push "plasmid sequencing primers changed to new format"
         end
       end
     end
