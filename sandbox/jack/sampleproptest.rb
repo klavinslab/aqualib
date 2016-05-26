3e3885bdb450672c5c2dbe6d9ad3ad435428136e
joonseokii <joonseokii@yahoo.com>
2016-05-26 06:37:36 -0700

    Update sampleproptest.rb

--- sandbox/jack/sampleproptest.rb
+++ sandbox/jack/sampleproptest.rb
@@ -13,7 +13,7 @@ class Protocol
     			}
 		end
 		show {
-			note test_samp.sample.properties["Bacterial Marker".to_sym]
+			note test_samp.sample.properties[:"Bacterial Marker".to_sym]
 		}
 	end
 end
