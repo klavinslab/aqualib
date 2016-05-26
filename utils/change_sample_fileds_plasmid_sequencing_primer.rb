7a6a814d04a41b3d8171f2fd739d35412d426df3
Yaoyu Yang <yaoyu.yang88@gmail.com>
2016-05-25 21:42:22 -0700

    update plasmid sample field change

--- utils/change_sample_fileds_plasmid_sequencing_primer.rb
+++ utils/change_sample_fileds_plasmid_sequencing_primer.rb
@@ -16,13 +16,15 @@ class Protocol
     changes = []
     plasmids.each do |plasmid|
       primer_ids_str = plasmid.properties["Sequencing_primer_ids"]
-      primer_ids = primer_ids_str.split(",").map { |s| s.to_i }
-      if primer_ids.all? { |i| i != 0 }
-        primers = primer_ids.collect { |id| find(:sample, id: id)[0] }
-        plasmid.properties["Sequencing Primers"] = primers
-        plasmid.save
-        if plasmid.errors.empty?
-          changes.push "plasmid sequencing primers changed to new format with #{primers.collect { |primer| primer.id}}"
+      if primer_ids_str && primer_ids_str.length > 0
+        primer_ids = primer_ids_str.split(",").map { |s| s.to_i }
+        if primer_ids.all? { |i| i != 0 }
+          primers = primer_ids.collect { |id| find(:sample, id: id)[0] }
+          plasmid.properties["Sequencing Primers"] = primers
+          plasmid.save
+          if plasmid.errors.empty?
+            changes.push "plasmid sequencing primers changed to new format with #{primers.collect { |primer| primer.id}}"
+          end
         end
       end
     end
