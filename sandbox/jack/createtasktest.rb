f3647bd9a990c3619ec1a882e8b575f5dc1ae44c
joonseokii <joonseokii@yahoo.com>
2016-06-24 13:52:50 -0700

    Update createtasktest.rb

--- sandbox/jack/createtasktest.rb
+++ sandbox/jack/createtasktest.rb
@@ -7,16 +7,16 @@ class Protocol
 	
 	def main
 		tp = TaskPrototype.where("name = 'Ecoli Transformation'")[0]
-		show {
-			note tp
-		}
-		t = Task.new(name: "Ecoli_Transformation_#{aliquot_batch.id}", 
-                specification: { "plasmid_item_ids Plasmid Stock|1 ng/µL Plasmid Stock|Gibson Reaction Result" => find(:item, { sample: { name: "SSJ128" } } )[0].id }.to_json, 
-                task_prototype_id: tp.id, 
-                status: "waiting", 
-                user_id: Job.find(jid).user_id,
-                budget_id: 1)
+		p = find(:item, { sample: { name: "SSJ128" } } )[0]
+		t = Task.new(
+			name: "test_jack", 
+                	specification: { "plasmid_item_ids Plasmid Stock|1 ng/µL Plasmid Stock|Gibson Reaction Result" => [p.id] }.to_json, 
+                	task_prototype_id: tp.id, 
+        		status: "waiting", 
+                	user_id: p.sample.user.id,
+                	budget_id: 1)
 		t.save
 		t.notify "Automatically created from Make E Comp Cells.", job_id: jid
+		
 	end
 end
