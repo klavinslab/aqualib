741733d3d5badacfb089d91087dff7a1b4655cc6
joonseokii <joonseokii@yahoo.com>
2016-06-24 17:16:30 -0700

    Create warm_LB_electrocompetent_ecoli_comp_cells.rb

---
+++ workflows/ecoli/warm_LB_electrocompetent_ecoli_comp_cells.rb
@@ -0,0 +1,25 @@
+class Protocol
+	
+	def arguments
+		{
+			io_hash: {}
+		}
+	end
+	
+	
+	def main
+		
+		io_hash = input[:io_hash]
+		
+		lb_liquid = find(:item, id: (io_hash[:lb_liquid]))[0]
+		take [lb_liquid], interactive: true
+		show {
+			title "Place LB in heat bath"
+			note "Set heat bath to 37"
+			note "Once temperature reaches 37, immerse LB in beads"
+		}
+		io_hash = {new_lb_liquid: lb_liquid.id}.merge(io_hash)
+		release([lb_liquid], interactive: true)
+		return { io_hash: io_hash }
+	end
+end
