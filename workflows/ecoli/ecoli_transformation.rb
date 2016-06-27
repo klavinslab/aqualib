28e25ae933123cb2579dd35ee5f2f95c060b5b61
GARRETT NEWMAN <garrett.s.newman@gmail.com>
2016-06-27 14:33:48 -0700

    Temporary ecoli_transformation fix

--- workflows/ecoli/ecoli_transformation.rb
+++ workflows/ecoli/ecoli_transformation.rb
@@ -76,22 +76,23 @@ class Protocol
     num = transformed_aliquots.length
     num_arr = *(1..num)
 
-    ecolibatch = find_batch(plasmid_items)
-    if ecolibatch.nil?
-      raise "No such E coli batch"
-    elsif ecolibatch.get("tested") == "No"
-      Item.find(ecolibatch.id).associate "tested", "Yes", upload=nil
-      matrix = Collection.find(ecolibatch).matrix
-      num_samp = Collection.find(ecolibatch).num_samples
-      row = num_samp / (matrix[0].length)
-      col = (num_samp - 1) % matrix[0].length    
-      # for debugging
-      #show {
-      #  note row
-      #  note col
-      #}
-      Collection.find(ecolibatch).set row, col, nil
-    end
+    # TODO: Fix e. coli batching so it doesn't reference plasmid_items[0] when nil
+    # ecolibatch = find_batch(plasmid_items)
+    # if ecolibatch.nil?
+    #   raise "No such E coli batch"
+    # elsif ecolibatch.get("tested") == "No"
+    #   Item.find(ecolibatch.id).associate "tested", "Yes", upload=nil
+    #   matrix = Collection.find(ecolibatch).matrix
+    #   num_samp = Collection.find(ecolibatch).num_samples
+    #   row = num_samp / (matrix[0].length)
+    #   col = (num_samp - 1) % matrix[0].length    
+    #   # for debugging
+    #   #show {
+    #   #  note row
+    #   #  note col
+    #   #}
+    #   Collection.find(ecolibatch).set row, col, nil
+    # end
 
     show {
       title "Prepare bench"
