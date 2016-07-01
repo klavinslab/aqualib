6ba4aca0824db85724594b37f576bfd27bb00cea
Garrett <garrett.s.newman@gmail.com>
2016-07-01 09:48:42 -0700

    refactor

--- workflows/ecoli/sequencing.rb
+++ workflows/ecoli/sequencing.rb
@@ -24,25 +24,22 @@ class Protocol
 
     total_vols_per_stock = total_volumes_by_item stocks, volumes
     verify_data = show {
-      title "Verify enough volume of each plasmid stock exists"
+      title "Verify enough volume of each #{stocks[0].object_type.name} exists"
       total_vols_per_stock.each { |id, v| 
-        select ["Yes", "No"], var: "#{id}", label: "Is there at least #{v.round(1)} µL of #{id}?", default: 0
+        select ["Yes", "No"], var: "#{id}", label: "Is there at least #{(v + 5).round(1)} µL of #{id}?", default: 0
       }
     }
 
-    enough_vol_stocks = stocks.select { |s| verify_data[:"#{s.id}".to_sym] == "Yes" }
-    not_enough_vol_stocks = stocks - enough_vol_stocks
+    # show {
+    #   note "verify_data"
+    #   note verify_data.map { |id, sel| "#{id}: #{sel}" }
+    #   note "enough"
+    #   note enough_vol_stocks.map { |s| s.id }
+    #   note "not enough"
+    #   note not_enough_vol_stocks.map { |s| s.id }
+    # }
 
-    show {
-      note "verify_data"
-      note verify_data.map { |id, sel| "#{id}: #{sel}" }
-      note "enough"
-      note enough_vol_stocks.map { |s| s.id }
-      note "not enough"
-      note not_enough_vol_stocks.map { |s| s.id }
-    }
-
-    [enough_vol_stocks, not_enough_vol_stocks]
+    stocks.map { |s| verify_data[:"#{s.id}".to_sym] == "Yes" ? true : false }
   end
 
   def arguments
@@ -154,12 +151,19 @@ class Protocol
     plasmid_volume_list.collect! { |v| ((v/0.2).ceil*0.2).round(1) }
     plasmid_volume_list.collect! { |v| v < 0.5 ? 0.5 : v > 12.5 ? 12.5 : v }
     water_volume_list = plasmid_volume_list.collect { |v| (((12.5-v)/0.2).floor*0.2).round(1) }
+    primer_volume_list = primer_aliquots.collect { |p| 2.5 }
 
-    water_with_volume = water_volume_list.collect { |v| v.to_s + " µL" }
-    plasmids_with_volume = plasmid_stock_ids.map.with_index { |pid,i| plasmid_volume_list[i].to_s + " µL of " + pid.to_s }
-    primers_with_volume = primer_aliquots.collect { |p| "2.5 µL of " + p.id.to_s }
+    water_with_volume = water_volume_list.map { |v| v.to_s + " µL" }
+    plasmids_with_volume = plasmid_stock_ids.map.with_index { |pid, i| plasmid_volume_list[i].to_s + " µL of " + pid.to_s }
+    primers_with_volume = primer_aliquots.map.with_index { |p, i| primer_volume_list[i].to_s + " µL of " + p.id.to_s }
 
-    enough_vol_stocks, not_enough_vol_stocks = verify_enough_volumes_each_stock plasmid_stocks, plasmid_volume_list
+    enough_plasmid_vol_bools = determine_enough_volumes_each_stock plasmid_stocks, plasmid_volume_list
+    enough_primer_vol_bools = verify_enough_volumes_each_stock primer_aliquots, primer_volume_list
+
+    show {
+      note enough_plasmid_vol_bools.length
+      note enough_primer_vol_bools.length
+    }
 
     stripwells = produce spread plasmid_stocks, "Stripwell", 1, 12
     show {
