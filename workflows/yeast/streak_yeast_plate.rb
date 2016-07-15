3ed95f682adb5c16e87829c2db8f4f8c475d5415
www-data <www-data@ip-172-31-41-121.us-west-2.compute.internal>
2016-07-14 21:59:34 -0700

    Repo::save: Updated aqualib/workflows/yeast/streak_yeast_plate.rb via the Aquarium developer tool.

--- workflows/yeast/streak_yeast_plate.rb
+++ workflows/yeast/streak_yeast_plate.rb
@@ -27,7 +27,7 @@ class Protocol
   def main
     io_hash = input[:io_hash]
     io_hash = input if !input[:io_hash] || input[:io_hash].empty?
-    #io_hash = { debug_mode: "No", yeast_overnight_ids: [], yeast_glycerol_stock_ids: [], yeast_selective_plate_types: [] }.merge io_hash
+    io_hash = { debug_mode: "No", yeast_overnight_ids: [], yeast_glycerol_stock_ids: [], yeast_selective_plate_types: [] }.merge io_hash
 
     if io_hash[:debug_mode].downcase == "yes"
       def debug
@@ -101,7 +101,7 @@ class Protocol
 
     # streak plate for yeast overnights if there is yeast_overnight_ids
 
-    if yeast_overnights.length > 0
+    if yeast_overnights.blank?
 
       yeast_overnights = io_hash[:yeast_overnight_ids].collect { |yid| find(:item, id: yid)[0] }
       overnight_streaked_yeast_plates = yeast_overnights.collect { |y| produce new_sample y.sample.name, of: "Yeast Strain", as: "Yeast Plate"}
@@ -112,8 +112,8 @@ class Protocol
           check "Grab one #{plate_type} plate and label with #{overnight_streaked_yeast_plates[idx].id}"
             
         end
-      } if io_hash[:yeast_selective_plate_types].length > 0
-      num_plates = io_hash[:yeast_selective_plate_types].length
+      } if io_hash[:yeast_selective_plate_types].blank?
+      num_plates = io_hash[:yeast_selective_plate_types].size
       update_batch_matrix plate_batch, plate_batch.num_samples - num_plates, "YPAD"
 
       take yeast_overnights, interactive: true
