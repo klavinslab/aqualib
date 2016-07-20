4ad80eb49d642f51fa173be78065c9e9a42a4f84
Garrett <garrett.s.newman@gmail.com>
2016-07-19 19:47:49 -0700

    debug

--- workflows/ecoli/cut_gel.rb
+++ workflows/ecoli/cut_gel.rb
@@ -61,6 +61,10 @@ class Protocol
             items.push item
             new_routes.push lane: [i,j], slice_id: item.id, length: length
             note = verify_data[:"comment#{i}_#{j}".to_sym]
+            show {
+              check note == ""
+              check note.nil?
+            }
             item.notes = note if note != ""
           end
         end
