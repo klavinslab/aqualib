d34c98d03c34843f4a0c49422dd363d5ee93f925
Ayesha Saleem <ayeshas@uw.edu>
2016-07-22 23:24:06 -0700

    checking handling of null case

--- workflows/yeast/make_antibiotic_plate.rb
+++ workflows/yeast/make_antibiotic_plate.rb
@@ -54,12 +54,12 @@ class Protocol
 
       if markers.include? marker
         overall_batches = find(:item, object_type: { name: "Agar Plate Batch" }).map{ |b| collection_from b }
-        plate_batch = overall_batches.find{ |b| !b.num_samples.zero? && find(:sample, id: b.matrix[0][0])[0].name == "YPAD" }
+        plate_batch = overall_batches.find{ |b| !b.num_samples.zero? && find(:sample, id: b.matrix[0][0])[0].name == "YPAD" } if !plate_batch.blank?
         num_plates = plate_batch.num_samples
         update_batch_matrix plate_batch, num_plates - num, "YPAD"
         if num_plates < num 
           num_left = num - num_plates
-          plate_batch_two = overall_batches.find{ |b| !b.num_samples.zero? && find(:sample, id: b.matrix[0][0])[0].name == "YPAD"} 
+          plate_batch_two = overall_batches.find{ |b| !b.num_samples.zero? && find(:sample, id: b.matrix[0][0])[0].name == "YPAD"} if !plate_batch.blank?
           update_batch_matrix plate_batch_two, plate_batch_two.num_samples - num_left, "YPAD"
         end
         show {
