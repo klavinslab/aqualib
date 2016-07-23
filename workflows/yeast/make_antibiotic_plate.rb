68c93ffbbc9fa9154a6311eb83a79754b9358513
Ayesha Saleem <ayeshas@uw.edu>
2016-07-22 23:31:47 -0700

    debugging

--- workflows/yeast/make_antibiotic_plate.rb
+++ workflows/yeast/make_antibiotic_plate.rb
@@ -55,8 +55,8 @@ class Protocol
       if markers.include? marker
         overall_batches = find(:item, object_type: { name: "Agar Plate Batch" }).map{ |b| collection_from b }
         plate_batch = overall_batches.find{ |b| !b.num_samples.zero? && find(:sample, id: b.matrix[0][0])[0].name == "YPAD" } 
-        num_plates = plate_batch.num_samples
         if plate_batch.present?
+          num_plates = plate_batch.num_samples
           update_batch_matrix plate_batch, num_plates - num, "YPAD" if plate_batch.present?
           if num_plates < num 
             num_left = num - num_plates
