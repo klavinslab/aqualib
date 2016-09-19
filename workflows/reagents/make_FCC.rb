cb080dc01e239a81e3823ff9ab42435dca1cab1a
cpc3 <cpc3@uw.edu>
2016-09-18 19:27:38 -0700

    FCC changes

--- workflows/reagents/make_FCC.rb
+++ workflows/reagents/make_FCC.rb
@@ -5,7 +5,6 @@ class Protocol
         
         glycerol = find(:item, { object_type: {name: "Glycerol"}}) [0]
         water = find( :item, {object_type: {name: "Molecular Biology Grade Water"}})
-		DMSO = find(:item, { object_type: { name: "Dimethyl Sulphoxide (DMSO)" } } )[0]
 		bottle = find(:item, { object_type: { name: "250 mL Bottle" } } )[0]
 
 		show {
@@ -13,7 +12,7 @@ class Protocol
 			note "This protocol makes #{media_type} for yeast transformations."
 		}
 
-		take [glycerol, water, DMSO, bottle], interactive: true
+		take [glycerol, water, dmso, bottle], interactive: true
 
 		show {
 			title "Measure out DMSO"
@@ -48,7 +47,7 @@ class Protocol
 
 		media.location = "To be autoclaved area"
 
-		release [glycerol, water, DMSO, media], interactive: true
+		release [glycerol, water, dmso, media], interactive: true
 		
 		release [bottle]
 	end
