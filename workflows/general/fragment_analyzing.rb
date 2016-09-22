734a28f5f7e939f0a345b8d5f01c432610494f00
Garrett <garrett.s.newman@gmail.com>
2016-09-22 16:04:14 -0700

    debug

--- workflows/general/fragment_analyzing.rb
+++ workflows/general/fragment_analyzing.rb
@@ -442,6 +442,7 @@ class Protocol
     verification_digest_task_ids.each do |tid|
       stripwell = verif_dig_stripwells.find { |sw| sw.datum[:task_id_mapping].include? tid }
       show { note stripwell.id }
+      show { note gel_uploads.keys }
       upload_id = gel_uploads[stripwell.id][:stripwell][0][:id]
       upload_url = Upload.find(upload_id).url
       associated_gel = collection_from stripwell.id
