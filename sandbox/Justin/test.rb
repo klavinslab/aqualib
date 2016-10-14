e4276b702b4846a4819bc03c71de38d9b1e166cb
jvrana <justin.vrana@gmail.com>
2016-10-13 18:00:36 -0700

    Update test.rb

--- sandbox/Justin/test.rb
+++ sandbox/Justin/test.rb
@@ -6,7 +6,9 @@ class Protocol
     to_be_added = [16204]
     to_be_added.each do |i|
       sample = find(:sample, id:i)
-      produce new_sample sample[:name], of: “Enzyme”, as: “Enzyme Stock”
+      j = sample[:name]
+#       j = produce new_sample "pLAB1",       of: "Plasmid",      as: "Plasmid Stock"
+#       j = produce new_sample sample[:name], of: “Enzyme”, as: “Enzyme Stock”
     end
   end
 end
