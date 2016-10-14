298b9263cb6e4b3622b16cfb9f646b1a8cda7231
jvrana <justin.vrana@gmail.com>
2016-10-13 18:11:02 -0700

    Update and rename test.rb to addenzymes.rb

---
+++ sandbox/Justin/addenzymes.rb
@@ -0,0 +1,13 @@
+# needs "aqualib/lib/standard"
+# needs "aqualib/lib/cloning"
+
+# class Protocol
+#   def main
+#     to_be_added = ['Taq DNA Ligase', 'Taq DNA Ligase', 'DpnI', 'Taq PCR Master Mix', 'Taq PCR Master Mix', 'Taq PCR Master Mix', 'Taq PCR Master Mix', 'Taq PCR Master Mix', 'Taq PCR Master Mix', 'Taq PCR Master Mix', 'Taq PCR Master Mix', 'iQ Supermix', 'phi29 DNA Polymerase', 'EcoRV', 'NheI', 'Taq DNA Ligase', 'Phusion Polymerase', 'DpnI', 'XhoI', 'NdeI', 'PmeI', 'T4 DNA Ligase', 'Phusion HF Master Mix', 'Phusion HF Master Mix', 'Phusion HF Master Mix', 'PmeI', 'DpnI', 'BsaI', 'T4 DNA Ligase', 'T5 exonuclease', 'Kapa HF Master Mix', 'Kapa HF Master Mix', 'T5 exonuclease', 'T4 Polynucleotide Kinase', 'Kapa HF Master Mix']
+#     to_be_added.each do |i|
+#       produce new_sample i, of: "Enzyme", as: "Enzyme Stock"
+# #       j = produce new_sample "pLAB1",       of: "Plasmid",      as: "Plasmid Stock"
+# #       j = produce new_sample sample[:name], of: “Enzyme”, as: “Enzyme Stock”
+#     end
+#   end
+# end
