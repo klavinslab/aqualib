20f2e5b4aae616516b7003fafe4662a1a84250e5
ayeshasal <ayeshas@uw.edu>
2016-10-25 17:09:15 -0700

    fixing bug

--- workflows/media/pour_agar_LB.rb
+++ workflows/media/pour_agar_LB.rb
@@ -31,9 +31,9 @@ class Protocol
         agar_media.push(made_media)
       end
     end
-    agar_media.each_with_index do |id|
+    for i in 1..(agar_media.length)
 
-      take [agar_media[id]], interactive: true
+      take [agar_media[i - 1]], interactive: true
 
       type = show {
         title "Select option"
