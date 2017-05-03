5c26b46578eb6e117ee9535512f6cc7a83763dc9
Garrett <garrett.s.newman@gmail.com>
2017-05-03 16:24:18 -0700

    started e coli colony PCR

---
+++ workflows/ecoli/ecoli_colony_PCR.rb
@@ -0,0 +1,182 @@
+needs "aqualib/lib/standard"
+needs "aqualib/lib/cloning"
+
+class Protocol
+
+  include Standard
+  include Cloning
+
+  def arguments
+    {
+      io_hash: {},
+      lysate_stripwell_ids: [22627],
+      debug_mode: "Yes"
+    }
+  end
+
+  def main
+    io_hash = input[:io_hash]
+    io_hash = input if !input[:io_hash] || input[:io_hash].empty?
+    io_hash = { lysate_stripwell_ids: [], debug_mode: "No" }.merge io_hash
+
+    if io_hash[:debug_mode].downcase == "yes"
+      def debug
+        true
+      end
+    end
+
+    # find and take lystae stripwells
+    lysate_stripwells = io_hash[:lysate_stripwell_ids].collect { |i| collection_from i }
+    take lysate_stripwells, interactive: true
+
+    # find yeast_samples, primer_aliquots, T Anneals
+    yeast_sample_ids = lysate_stripwells.collect { |i| i.matrix }
+    yeast_samples = yeast_sample_ids.collect do |y|
+      y.flatten!.delete(-1)
+      y.collect { |y| find(:sample, id: y)[0] }
+    end
+
+    # dilute primer stocks if needed
+    forward_primer_ids = yeast_samples.collect { |y| y.collect { |x| x.properties["QC Primer1"].id } }.flatten
+    reverse_primer_ids = yeast_samples.collect { |y| y.collect { |x| x.properties["QC Primer2"].id } }.flatten
+    diluted_primer_aliquots = dilute_samples primers_need_to_dilute(forward_primer_ids + reverse_primer_ids)
+
+    forward_primers = yeast_samples.collect { |y| y.collect { |x| x.properties["QC Primer1"].in("Primer Aliquot")[0]} }
+    reverse_primers = yeast_samples.collect { |y| y.collect { |x| x.properties["QC Primer2"].in("Primer Aliquot")[0]} }
+    tanneals = forward_primers.map.with_index { |pr, idx1| pr.map.with_index { |p, idx2| ( p.sample.properties["T Anneal"] + reverse_primers[idx1][idx2].sample.properties["T Anneal"] ) / 2 } }
+
+    primers = (forward_primers.flatten + reverse_primers.flatten).uniq
+
+    # take primer aliquots
+    take primers - diluted_primer_aliquots, interactive: true, method: "boxes"
+
+    # get kapa enzyme
+    kapa_stock_item =  find(:sample, name: "Kapa HF Master Mix")[0].in("Enzyme Stock")[0]
+    take [kapa_stock_item], interactive: true, method: "boxes"
+
+    # build a pcrs hash that pcrs by T Anneal
+    pcrs = Hash.new { |h, k| h[k] = { yeast_samples: [], forward_primers: [], reverse_primers: [], lysate_stripwells: [], pcr_stripwells: [], tanneals: [] } }
+
+    lysate_stripwells.each_with_index do |sw, idx|
+      if tanneals[idx].min >= 70
+        key = :t70
+      elsif tanneals[idx].min >= 67
+        key = :t67
+      else
+        key = :t64
+      end
+      pcrs[key][:yeast_samples].push yeast_samples[idx]
+      pcrs[key][:lysate_stripwells].push sw
+      pcrs[key][:forward_primers].push forward_primers[idx]
+      pcrs[key][:reverse_primers].push reverse_primers[idx]
+      pcrs[key][:tanneals].push tanneals[idx].min.round(0)
+    end
+
+    # produce pcr stripwells
+    pcrs.each do |t, pcr|
+      pcr[:lysate_stripwells].each do |sw|
+        pcr_stripwell = produce new_collection "Stripwell", 1, 12
+        pcr_stripwell.matrix = sw.matrix
+        pcr_stripwell.save
+        pcr[:pcr_stripwells].push pcr_stripwell
+      end
+    end
+    pcr_stripwells = pcrs.collect { |t, pcr| pcr[:pcr_stripwells] }
+    pcr_stripwells.flatten!
+
+    # set up pcr stripwells
+    show {
+      title "Prepare Stripwell Tubes"
+      pcrs.each do |t, pcr|
+        pcr[:pcr_stripwells].each_with_index do |sw, idx|
+          if sw.num_samples <= 6
+            check "Grab a new stripwell with 6 wells and label with the id #{sw}."
+          else
+            check "Grab a new stripwell with 12 wells and label with the id #{sw}."
+          end
+          check "Pipette 3.5 µL of molecular grade water into wells " + sw.non_empty_string + "."
+        end
+      end
+    }
+
+    show {
+      title "Add Template"
+      pcrs.each do |t, pcr|
+        pcr[:pcr_stripwells].each_with_index do |sw, idx|
+      check "Spin down stripwell #{pcr[:lysate_stripwells][idx]} immediately before adding to new stipwell."
+      check "Transfer 0.5 µL from each well in stripwell #{pcr[:lysate_stripwells][idx]} to the new stripwell #{sw}"
+      warning "Spin down stipwell from lysate protocol right before adding to PCR stripwell."
+        end
+      end
+    }
+
+    # add primers to stripwells
+    pcrs.each do |t, pcr|
+      pcr[:pcr_stripwells].each_with_index do |sw, idx|
+        load_samples( [ "Forward Primer, 0.5 µL", "Reverse Primer, 0.5 µL" ], [
+            pcr[:forward_primers][idx],
+            pcr[:reverse_primers][idx]
+          ], [sw] ) {
+            warning "Use a fresh pipette tip for each transfer.".upcase
+          }
+      end
+    end
+
+    # Add master mix
+    show {
+      title "Add Master Mix"
+      pcr_stripwells.each do |sw|
+        check "Pipette 5 µL of master mix (item #{kapa_stock_item}) into each of wells " + sw.non_empty_string + " of stripwell #{sw}."
+      end
+      separator
+      warning "USE A NEW PIPETTE TIP FOR EACH WELL AND PIPETTE UP AND DOWN TO MIX"
+    }
+
+    release [kapa_stock_item], interactive: true, method: "boxes"
+
+    # run the thermocycler
+    pcrs.each do |key, pcr|
+      tanneal = pcr[:tanneals].inject { |sum, el| sum + el }.to_f / pcr[:tanneals].size
+      thermocycler = show {
+        title "Start the PCRs at #{tanneal.round(0)} C"
+        check "Place the stripwells #{pcr[:pcr_stripwells].collect { |sw| sw.id } } into an available thermal cycler and close the lid."
+        get "text", var: "name", label: "Enter the name of the thermocycler used", default: "TC1"
+        separator
+        check "Click 'Home' then click 'Saved Protocol'. Choose 'YY' and then 'COLONYPCR'."
+        check "Set the anneal temperature to #{tanneal.round(0)}. This is the 3rd temperature."
+        check "Set the 4th time (extension time) to be to be 2 minutes."
+        check "Press 'run' and select 10 µL."
+        #image "thermal_cycler_select"
+      }
+      pcr[:pcr_stripwells].each do |sw|
+        sw.move thermocycler[:name]
+      end
+    end
+
+    # Release the pcr_stripwells silently, since they should stay in the thermocycler
+    release pcr_stripwells
+
+    show {
+      title "Clean up"
+      note "Discard the following stripwells"
+      note "#{lysate_stripwells.collect { |sw| sw.id }}"
+      lysate_stripwells.each do |sw|
+        sw.mark_as_deleted
+      end
+    }
+
+    release primers, interactive: true, method: "boxes"
+
+    if io_hash[:task_ids]
+      io_hash[:task_ids].each do |tid|
+        task = find(:task, id:tid)[0]
+        set_task_status(task,"pcr")
+      end
+    end
+
+    io_hash[:stripwell_ids] = pcr_stripwells.collect { |s| s.id }
+    return { io_hash: io_hash }
+
+  end
+
+end
---
+++ workflows/ecoli/make_ecoli_lysate.rb
@@ -0,0 +1,163 @@
+needs "aqualib/lib/standard"
+needs "aqualib/lib/cloning"
+
+class Protocol
+
+  include Standard
+  include Cloning
+
+  def arguments
+    {
+      io_hash: {},
+      yeast_plate_ids: [32170,32171],
+      num_colonies: [3,3],
+      debug_mode: "No",
+      group: "cloning"
+    }
+  end
+
+  def main
+    io_hash = input[:io_hash]
+    io_hash = input if !input[:io_hash] || input[:io_hash].empty?
+
+    # set default values
+    io_hash = { yeast_plate_ids: [], num_colonies: [], debug_mode: "No", comb_1: "10 thin", comb_2: "10 thin", volume: 10 }.merge io_hash
+
+    if io_hash[:debug_mode].downcase == "yes"
+      def debug
+        true
+      end
+    end
+
+    raise "Incorrect inputs, yeast_plate_ids size does not match num_colonies size. They need to be one to one correspondence." if io_hash[:yeast_plate_ids].length != io_hash[:num_colonies].length
+
+    yeast_items = io_hash[:yeast_plate_ids].collect {|yid| find(:item, id: yid )[0]}
+
+    show {
+      title "Protocol information"
+      note "This protocol makes yeast lysates in stripwell tubes for the following plates:"
+      note yeast_items.join(", ")
+    }
+
+    take yeast_items, interactive: true
+
+    yeast_samples = []
+    yeast_colonies = []
+    yeast_items.each_with_index do |y,idx|
+      start_colony = (y.datum[:QC_result] || []).length
+      (1..io_hash[:num_colonies][idx]).each do |x|
+        yeast_samples.push y.sample
+        yeast_colonies.push "#{y.id}.c#{start_colony+x}"
+      end
+    end
+    
+
+    # build a pcrs hash that group fragment pcr by T Anneal
+    pcrs = Hash.new { |h, k| h[k] = { yeast_samples: [], yeast_colonies: [], forward_primers: [], reverse_primers: [], stripwells: [] } }
+
+    forward_primers = yeast_samples.collect { |y| y.properties["QC Primer1"] }
+    reverse_primers = yeast_samples.collect { |y| y.properties["QC Primer2"] }
+
+    yeast_samples.each_with_index do |y, idx|
+      tanneal = (forward_primers[idx].properties["T Anneal"] + reverse_primers[idx].properties["T Anneal"])/2
+      if tanneal >= 70
+        pcrs[70][:yeast_samples].push y
+        pcrs[70][:yeast_colonies].push yeast_colonies[idx]
+      elsif tanneal >= 67
+        pcrs[67][:yeast_samples].push y
+        pcrs[67][:yeast_colonies].push yeast_colonies[idx]
+      else
+        pcrs[64][:yeast_samples].push y
+        pcrs[64][:yeast_colonies].push yeast_colonies[idx]
+      end
+    end
+
+    pcrs.each do |t, pcr|
+      pcr[:stripwells] = produce spread pcr[:yeast_samples], "Stripwell", 1, 12
+      pcr[:forward_primers] = pcr[:yeast_samples].collect { |y| y.properties["QC Primer1"] }
+      pcr[:reverse_primers] = pcr[:yeast_samples].collect { |y| y.properties["QC Primer1"] }
+    end
+
+    stripwells = pcrs.collect { |t, pcr| pcr[:stripwells] }
+    stripwells.flatten!
+
+    show {
+      title "Prepare Stripwell Tubes"
+      stripwells.each do |sw|
+        if sw.num_samples <= 6
+          check "Grab a new stripwell with 6 wells and label with the id #{sw}."
+        else
+          check "Grab a new stripwell with 12 wells and label with the id #{sw}."
+        end
+        note "Pipette 25 µL of 20 mM NaOH into wells " + sw.non_empty_string + "."
+        warning "Using 25 µL NaOH, not 30 µL SDS.  "
+      end
+      # TODO: Put an image of a labeled stripwell here
+    }
+
+    # add colonies to stripwells
+    pcrs.each do |t, pcr|
+      load_samples_variable_vol( [ "Colony cx from plate, 1/3 size" ], [
+          pcr[:yeast_colonies],
+        ], pcr[:stripwells] ) {
+        note "For each plate id.cx (x = 1,2,3,...), if a colony cx is not marked on the plate, mark it with a circle and write done cx (x = 1,2,3,...) nearby. If a colony cx is alread marked on the plate, scrape that colony."
+        note "Use a sterile 10 µL tip to scrape about 1/3 of the marked colony, swirl tip inside the well until mixed."
+      }
+    end
+
+    # Run the thermocycler
+    thermocycler = show {
+      title "Start the lysate reactions"
+      check "Put the cap on each stripwell #{stripwells.collect { |sw| sw.id } }. Press each one very hard to make sure it is sealed."
+      check "Vortex all the stripwells on a green tube holder on a vortexer."
+      check "Place the stripwells into an available thermal cycler and close the lid."
+      get "text", var: "name", label: "Enter the name of the thermocycler used", default: "TC1"
+      separator
+      check "Click 'Home' then click 'Saved Protocol'. Choose 'YY' and then 'LYSATE'."
+      check "Press 'run' and select 25 µL."
+      # TODO: image: "thermal_cycler_home"
+    }
+
+    # Set the location of the stripwells to be the name of the thermocycler
+    stripwells.each do |sw|
+      sw.move thermocycler[:name]
+    end
+
+    release stripwells
+    release yeast_items, interactive: true
+
+    show {
+      title "Wait for 10 minutes"
+      timer initial: { hours: 0, minutes: 10, seconds: 0}
+    }
+
+    take stripwells, interactive: true
+
+    show {
+      title "Keep stripwells"
+          check "Keep the new stripwell on the bench for the next protocol to use."
+          warning "DO NOT SPIN DOWN STRIPWELLS."
+    }
+
+    stripwells.each do |sw|
+      sw.move "Bench"
+    end
+
+    release stripwells
+
+    if io_hash[:task_ids]
+      io_hash[:task_ids].each do |tid|
+        task = find(:task, id:tid)[0]
+        set_task_status(task,"lysate")
+      end
+    end
+
+    io_hash[:lysate_stripwell_ids] = stripwells.collect { |sw| sw.id }
+
+    # To let metacol know whether to fire "move cartridge" protocol
+    cartridge_in_analyzer = find(:item, object_type: { name: "QX DNA Screening Cartridge" }).select { |c| c.location.downcase == "fragment analyzer" }.any?
+    
+    return { io_hash: io_hash, cartridge_in_analyzer: cartridge_in_analyzer }
+  end
+
+end
