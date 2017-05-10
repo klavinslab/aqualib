044e89e1639a42b17de44e717b7144857e62e95c
Garrett <garrett.s.newman@gmail.com>
2017-05-10 15:35:58 -0700

    start yeast_QC_success

--- data_scrubbing/yeast_QC_success.rb
+++ data_scrubbing/yeast_QC_success.rb
@@ -10,70 +10,73 @@ class Protocol
 	def main
 		# Find Primer Orders
 		yeast_QC_prot_id = TaskPrototype.where(name: "Yeast Strain QC").first.id
-		tasks_response = Test.send({
-		  login: Test.login,
-		  key: Test.key,
-		  run: {
-		    method: "find",
-		    args: {
-		      model: :task,
-		      where: { id: lower_bound_id..upper_bound_id,
-		      		   task_prototype_id: yeast_QC_prot_id }
-		    }
-		  }
-		})
-		tasks = tasks_response[:rows]
+		# tasks_response = Test.send({
+		#   login: Test.login,
+		#   key: Test.key,
+		#   run: {
+		#     method: "find",
+		#     args: {
+		#       model: :task,
+		#       where: { id: lower_bound_id..upper_bound_id,
+		#       		   task_prototype_id: yeast_QC_prot_id }
+		#     }
+		#   }
+		# })
+		# tasks = tasks_response[:rows]
 		puts "TASKS FETCHED..."
+		tasks = find(:task, task_prototype_id: yeast_QC_prot_id)
+		puts Task
+		puts tasks
 
-		# Build hash for each task
-		task_hashes = tasks.map do |task|
-			# yeast_plates
-			yeast_plate_ids = task[:specification][/\[(.*?)\]/, 1].split(',').map { |id| id.to_i }
+		# # Build hash for each task
+		# task_hashes = tasks.map do |task|
+		# 	# yeast_plates
+		# 	yeast_plate_ids = task[:specification][/\[(.*?)\]/, 1].split(',').map { |id| id.to_i }
 
-			plate_response = Test.send({
-			  login: Test.login,
-			  key: Test.key,
-			  run: {
-			    method: "find",
-			    args: {
-			      model: :item,
-			      where: { id: yeast_plate_ids }
-			    }
-			  }
-			})
-			yeast_plates = plate_response[:rows]
+		# 	plate_response = Test.send({
+		# 	  login: Test.login,
+		# 	  key: Test.key,
+		# 	  run: {
+		# 	    method: "find",
+		# 	    args: {
+		# 	      model: :item,
+		# 	      where: { id: yeast_plate_ids }
+		# 	    }
+		# 	  }
+		# 	})
+		# 	yeast_plates = plate_response[:rows]
 
-			# QC_results
-			qc_results = yeast_plates.map { |yp| yp[:data][:QC_result] }.flatten
+		# 	# QC_results
+		# 	qc_results = yeast_plates.map { |yp| yp[:data][:QC_result] }.flatten
 
-			# marker
-			yeast_sample_id = yeast_plates.first[:sample_id]
-			sample_response = Test.send({
-			  login: Test.login,
-			  key: Test.key,
-			  run: {
-			    method: "find",
-			    args: {
-			      model: :sample,
-			      where: { id: yeast_sample_id }
-			    }
-			  }
-			})
-			yeast_sample = sample_response[:rows].first
-			markers = yeast_sample[:fields][:"Integrated Marker(s)".to_sym]
-			puts yeast_sample.keys
+		# 	# marker
+		# 	yeast_sample_id = yeast_plates.first[:sample_id]
+		# 	sample_response = Test.send({
+		# 	  login: Test.login,
+		# 	  key: Test.key,
+		# 	  run: {
+		# 	    method: "find",
+		# 	    args: {
+		# 	      model: :sample,
+		# 	      where: { id: yeast_sample_id }
+		# 	    }
+		# 	  }
+		# 	})
+		# 	yeast_sample = sample_response[:rows].first
+		# 	markers = yeast_sample[:fields][:"Integrated Marker(s)".to_sym]
+		# 	puts yeast_sample.keys
 
-			{ task: task, yeast_plates: yeast_plates, QC_results: qc_results, markers: markers }
-		end
+		# 	{ task: task, yeast_plates: yeast_plates, QC_results: qc_results, markers: markers }
+		# end
 
-		task_hashes.select.each do |task_hash|
-			task = task_hash[:task]
-			puts "Task #{task[:id]}: #{task[:name]}"
-			puts "  date: #{task[:updated_at]}"
-			puts "  yeast plate ids: #{task_hash[:yeast_plates].map { |yp| yp[:id] }}"
-			puts "  QC_results: #{task_hash[:QC_results]}"
-			puts "  marker: #{task_hash[:markers]}"
-		end
+		# task_hashes.select.each do |task_hash|
+		# 	task = task_hash[:task]
+		# 	puts "Task #{task[:id]}: #{task[:name]}"
+		# 	puts "  date: #{task[:updated_at]}"
+		# 	puts "  yeast plate ids: #{task_hash[:yeast_plates].map { |yp| yp[:id] }}"
+		# 	puts "  QC_results: #{task_hash[:QC_results]}"
+		# 	puts "  marker: #{task_hash[:markers]}"
+		# end
 
 
 		# # Calculate average Primer cost
