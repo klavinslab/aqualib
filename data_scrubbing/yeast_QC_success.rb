762e3d22a48b7ba789d200e9b79714218748effd
Garrett <garrett.s.newman@gmail.com>
2017-05-10 16:13:50 -0700

    more yeast_QC_success

--- data_scrubbing/yeast_QC_success.rb
+++ data_scrubbing/yeast_QC_success.rb
@@ -10,33 +10,34 @@ class Protocol
 	def main
 		# Find Primer Orders
 		yeast_QC_prot_id = TaskPrototype.where(name: "Yeast Strain QC").first.id
-		tasks = Task.where({task_prototype_id: yeast_QC_prot_id, id: input[:id_bounds].first..input[:id_bounds].last})
+		tasks = Task.where({ task_prototype_id: yeast_QC_prot_id, id: input[:id_bounds].first..input[:id_bounds].last })
 		
 		puts "\nTASKS FETCHED\n-------------"
 		puts "#{tasks.length} Yeast Strain QC tasks"
 
 		# Build hash for each task
 		task_hashes = tasks.map do |task|
-			puts "\ntask #{task.id}"
+			puts "\ntask #{task.id}";
 			# yeast_plates
-			yeast_plate_ids = task.simple_spec[:yeast_plate_ids]
-			yeast_plates = Item.where(id: yeast_plate_ids)
-			puts "	yeast plates: #{yeast_plate_ids}"
+			yeast_plate_ids = task.simple_spec[:yeast_plate_ids];
+			yeast_plates = Item.where(id: yeast_plate_ids);
+			puts "	yeast plates: #{yeast_plate_ids}";
 
 			# QC_results
-			qc_results = yeast_plates.map { |yp| yp.datum[:QC_result] }.flatten
-			puts "	QC results: #{qc_results}"
+			qc_results = yeast_plates.map { |yp| yp.datum[:QC_result] }.flatten;
+			puts "	QC results: #{qc_results}";
 
 			# marker
-			yeast_sample = yeast_plates.first.sample
-			markers = yeast_sample.properties["Integrated Marker(s)"]
-			puts "	markers: #{markers}"
+			yeast_sample = yeast_plates.first.sample;
+			markers = yeast_sample.properties["Integrated Marker(s)"];
+			puts "	markers: #{markers}";
 
-			{ task: task, yeast_plates: yeast_plates, QC_results: qc_results, markers: markers }
+			{ task: task, yeast_plates: yeast_plates, QC_results: qc_results, markers: markers };
 		end
 
+		# Build QC success frequency CSV for TRP plates
 		task_hashes_trp = task_hashes.select { |th| th[:markers].downcase.include? "trp" }
-		puts task_hashes.length
-		puts task_hashes_trp.length
+		trp_success = task_hashes_trp.map { |th| { date: th[:task].created_at, success: th[:QC_results].count { |r| r == "Yes" } / th[:QC_results].length } }
+		puts trp_success
 	end
 end
