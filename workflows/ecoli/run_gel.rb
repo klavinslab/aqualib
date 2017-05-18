01c587bf47b6837c3ca8003e120b5f46f8dc704b
michelleparks <mnparks@uw.edu>
2017-05-18 10:30:38 -0700

    Update run_gel.rb

--- workflows/ecoli/run_gel.rb
+++ workflows/ecoli/run_gel.rb
@@ -112,13 +112,6 @@ class Protocol
           stripwell.mark_as_deleted
         end
       }
-      
-       show {
-        title "Set a timer"
-        check "When you get back to your bench, set a 40 minute timer." 
-        check "When the 40 minute timer is up, grab a lab manager to check on the gel. The lab manager may have you set another timer after checking the gel."
-         end
-         }
 
       release gels
       release [ ladder, dye ], interactive: true
