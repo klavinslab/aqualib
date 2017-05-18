6b6ec8c8bd1dbac291fefbdaf4101d3753090a9a
michelleparks <mnparks@uw.edu>
2017-05-18 10:47:18 -0700

    Update run_gel.rb

--- workflows/ecoli/run_gel.rb
+++ workflows/ecoli/run_gel.rb
@@ -110,13 +110,13 @@ class Protocol
         note "Discard the empty stripwells."  
         stripwells.each do |stripwell|
         stripwell.mark_as_deleted
+        end
        }
   
       show {
         title "Set a timer"
          check "When you get back to your bench, set a 40 minute timer." 
          check "When the 40 minute timer is up, grab a lab manager to check on the gel. The lab manager may have you set another timer after checking the gel."
-        end
       }
 
       release gels
